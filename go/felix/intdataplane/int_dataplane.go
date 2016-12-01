0aeaad5bfcce859ec7a2cd5c5207a4e6d9fc3f65
Matt <matt@projectcalico.org>
2016-12-01 13:11:56 -0800

    IPFIX emission

--- go/felix/intdataplane/int_dataplane.go
+++ go/felix/intdataplane/int_dataplane.go
@@ -15,12 +15,16 @@
 package intdataplane
 
 import (
+	"net"
 	"os"
+	"signal"
+	"syscall"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/projectcalico/felix/go/felix/collector"
 	"github.com/projectcalico/felix/go/felix/collector/stats"
 	"github.com/projectcalico/felix/go/felix/ifacemonitor"
+	"github.com/projectcalico/felix/go/felix/ipfix"
 	"github.com/projectcalico/felix/go/felix/ipsets"
 	"github.com/projectcalico/felix/go/felix/iptables"
 	"github.com/projectcalico/felix/go/felix/proto"
@@ -154,17 +158,21 @@ func (d *InternalDataplane) Start() {
 	conntrackDataSource.Start()
 
 	nfIngressSink := make(chan stats.StatUpdate)
-	nflogIngressDataSource := collector.NewNflogDataSource(nfIngressSink, 9597, stats.DirIn)
+	nflogIngressDataSource := collector.NewNflogDataSource(nfIngressSink, 1, stats.DirIn)
 	nflogIngressDataSource.Start()
 
 	nfEgressSink := make(chan stats.StatUpdate)
-	nflogEgressDataSource := collector.NewNflogDataSource(nfEgressSink, 9598, stats.DirOut)
+	nflogEgressDataSource := collector.NewNflogDataSource(nfEgressSink, 2, stats.DirOut)
 	nflogEgressDataSource.Start()
+
+	ipfixExportSink := make(chan ipfix.ExportRecord)
+	ipfixExporter := ipfix.NewIPFIXExporter(net.ParseIP("127.0.0.1"), 4739, "udp", ipfixExportSink)
+	ipfixExporter.Start()
 
 	printSink := make(chan *stats.Data)
 	datasources := []<-chan stats.StatUpdate{ctSink, nfIngressSink, nfEgressSink}
 	datasinks := []chan<- *stats.Data{printSink}
-	statsCollector := collector.NewCollector(datasources, datasinks)
+	statsCollector := collector.NewCollector(datasources, datasinks, ipfixExportSink)
 	statsCollector.Start()
 
 	sigChan := make(chan os.Signal, 1)
@@ -177,12 +185,11 @@ func (d *InternalDataplane) Start() {
 		}
 	}()
 
-	// TODO (Matt): Replace with ipfix emit code once I'm happy up to here.
 	go func() {
 		for data := range printSink {
 			log.Info("MD4 test output data: ", data)
 		}
-	}
+	}()
 }
 
 // onIfaceStateChange is our interface monitor callback.  It gets called from the monitor's thread.
