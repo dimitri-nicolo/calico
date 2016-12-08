591182a660ebd9d73f584979d8fae6f0035f6475
Matt <matt@projectcalico.org>
2016-12-08 08:35:02 -0800

    Temporary fix: remove bugged debug only code from Felix (will be cleaned up fully when we merge the upstream fix in)

--- go/felix/calc/policy_sorter.go
+++ go/felix/calc/policy_sorter.go
@@ -97,23 +97,7 @@ func (poc *PolicySorter) Sorted() []*tierInfo {
 			tierInfo.OrderedPolicies = append(tierInfo.OrderedPolicies,
 				PolKV{Key: k, Value: v})
 		}
-		if log.GetLevel() >= log.DebugLevel {
-			names := make([]string, len(tierInfo.OrderedPolicies))
-			for ii, kv := range tierInfo.OrderedPolicies {
-				names[ii] = fmt.Sprintf("%v(%v)",
-					kv.Key.Name, *kv.Value.Order)
-			}
-			log.Infof("Before sorting policies: %v", names)
-		}
 		sort.Sort(PolicyByOrder(tierInfo.OrderedPolicies))
-		if log.GetLevel() >= log.DebugLevel {
-			names := make([]string, len(tierInfo.OrderedPolicies))
-			for ii, kv := range tierInfo.OrderedPolicies {
-				names[ii] = fmt.Sprintf("%v(%v)",
-					kv.Key.Name, *kv.Value.Order)
-			}
-			log.Infof("After sorting policies: %v", names)
-		}
 	}
 	return tiers
 }
