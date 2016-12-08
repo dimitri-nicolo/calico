db79b23720e151ac402e460823151bbf56e1e25f
Matt <matt@projectcalico.org>
2016-12-08 08:39:42 -0800

    Unused import

--- go/felix/calc/policy_sorter.go
+++ go/felix/calc/policy_sorter.go
@@ -16,7 +16,6 @@ package calc
 
 import (
 	"fmt"
-	log "github.com/Sirupsen/logrus"
 	"github.com/projectcalico/libcalico-go/lib/backend/api"
 	"github.com/projectcalico/libcalico-go/lib/backend/model"
 	"sort"
