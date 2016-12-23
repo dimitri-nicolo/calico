28d6e96fb39872179cb700c86311b4bab3c9358d
Lance Robson <lance@tigera.io>
2016-12-23 19:57:10 +0000

    Add tiered workload policy test

--- tests/st/enterprise/test_enterprise.py
+++ tests/st/enterprise/test_enterprise.py
@@ -45,120 +45,12 @@ else:
     ADDITIONAL_DOCKER_OPTIONS = "--cluster-store=etcd://%s:2379 " % \
                                 get_ip()
 
-POLICY_DIR = "/calico/v1/policy/tier"
-# Canned policies, all of which should apply to all the computes.  We use
-# a variety of selectors just to hit different code paths.
-POLICY_ALLOW_CALICO_TRAFFIC_COMPUTE = {
-    "selector": "role == 'compute'",
-    "order": 0,
-    "inbound_rules": [
-        {"protocol": "tcp", "dst_ports": [
-            179,    # BGP
-            2379,   # etcd
-            2380,   # etcd
-            4001,   # etcd
-            5672],  # AMQP
-         "action": "allow",
-         "dst_selector": "role == 'compute' || role == 'control'"},
-        # Otherwise defer to next tier.
-        {"action": "next-tier"},
-    ],
-    "outbound_rules": [
-        {"protocol": "tcp", "dst_ports": [
-            80],
-         "action": "allow",
-         "dst_selector": "role == 'compute' || role == 'control'"},
-        {"action": "next-tier"},
-    ]
-}
-POLICY_ALLOW_CALICO_TRAFFIC_CONTROL = {
-    "selector": "role == 'control'",
-    "order": 0,
-    "inbound_rules": [
-        {"protocol": "tcp",
-         "dst_ports": [
-            80,     # Horizon UI
-            443,    # Horizon UI
-            2379,   # etcd
-            2380,   # etcd
-            4001,   # etcd
-            5672,   # AMQP
-            5000,   # keystone
-            8773,   # nova
-            8774,   # nova
-            8775,   # nova
-            9191,   # glance registry
-            9292,   # glance api
-            9696,   # neutron
-            35357   # keystone (openstack api)
-            ],
-         "action": "allow",
-         "dst_selector": "role == 'control'",
-         },
-        {"protocol": "udp", "action": "allow"},
-        # Otherwise defer to next tier.
-        {"action": "next-tier"},
-    ],
-    "outbound_rules": [
-        {"protocol": "tcp",
-         "dst_ports": [
-            2379,    # etcd
-            2380,    # etcd
-            4001,    # etcd
-            5672],   # AMQP
-         "action": "allow",
-         "dst_selector": "role == 'compute' || role == 'control'",
-         },
-        {"protocol": "udp", "action": "allow"},
-        {"action": "next-tier"},
-    ]
-}
-POLICY_ALLOW_ALL = {
-    "selector": "role == 'compute'",
-    "order": 10,
-    "inbound_rules": [
-        {"action": "allow"}
-    ],
-    "outbound_rules": [
-        {"action": "allow"}
-    ]
-}
-POLICY_DENY_ALL = {
-    "selector": "role != 'control'",
-    "order": 10,
-    "inbound_rules": [
-        {"action": "deny"}
-    ],
-    "outbound_rules": [
-        {"action": "deny"}
-    ]
-}
-POLICY_NEXT_ALL = {
-    "selector": "role != 'control' && role == 'compute'",
-    "order": 10,
-    "inbound_rules": [
-        {"action": "next-tier"}
-    ],
-    "outbound_rules": [
-        {"action": "next-tier"}
-    ]
-}
-POLICY_NONE_ALL = {
-    "selector": "all()",
-    "order": 10,
-    "inbound_rules": [
-    ],
-    "outbound_rules": [
-    ]
-}
 
-@staticmethod
 def parallel_host_setup(num_hosts):
     makehost = functools.partial(DockerHost,
                                  additional_docker_options=ADDITIONAL_DOCKER_OPTIONS,
                                  post_docker_commands=POST_DOCKER_COMMANDS,
                                  start_calico=False)
-
     hostnames = []
     for i in range(num_hosts):
         hostnames.append("host%s" % i)
@@ -168,11 +60,11 @@ def parallel_host_setup(num_hosts):
     pool.join()
     return hosts
 
+
 class MultiHostIpfix(TestBase):
     @classmethod
     def setUpClass(cls):
         super(TestBase, cls).setUpClass()
-        num_hosts = 2
         cls.hosts = []
         cls.hosts.append(DockerHost("host1",
                                     additional_docker_options=ADDITIONAL_DOCKER_OPTIONS,
@@ -327,3 +219,189 @@ class MultiHostIpfix(TestBase):
         self.mon.assert_flows_present(ping_flows, 20, allow_others=False)
 
 
+class TieredPolicyWorkloads(TestBase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestBase, cls).setUpClass()
+        cls.hosts = []
+        cls.hosts.append(DockerHost("host1",
+                                    additional_docker_options=ADDITIONAL_DOCKER_OPTIONS,
+                                    post_docker_commands=POST_DOCKER_COMMANDS,
+                                    start_calico=False))
+        cls.hosts.append(DockerHost("host2",
+                                    additional_docker_options=ADDITIONAL_DOCKER_OPTIONS,
+                                    post_docker_commands=POST_DOCKER_COMMANDS,
+                                    start_calico=False))
+        for host in cls.hosts:
+            host.start_calico_node()
+
+        cls.networks = []
+        cls.networks.append(cls.hosts[0].create_network("testnet1"))
+
+        cls.n1_workloads = []
+        # Create two workloads on cls.hosts[0] and one on cls.hosts[1] all in network 1.
+        cls.n1_workloads.append(cls.hosts[1].create_workload("workload_h2n1_1",
+                                                             image="workload",
+                                                             network=cls.networks[0]))
+        cls.n1_workloads.append(cls.hosts[0].create_workload("workload_h1n1_1",
+                                                             image="workload",
+                                                             network=cls.networks[0]))
+        # Assert that endpoints are in Calico
+        assert_number_endpoints(cls.hosts[0], 1)
+        assert_number_endpoints(cls.hosts[1], 1)
+
+    @classmethod
+    def tearDownClass(cls):
+        # Tidy up
+        for host in cls.hosts:
+            host.remove_workloads()
+        for network in cls.networks:
+            network.delete()
+        for host in cls.hosts:
+            host.cleanup()
+            del host
+
+    def test_tier_ordering_explicit(self):
+        """Check correct ordering of tiers by their explicit order field."""
+        self._do_tier_order_test("tier-c", 1,
+                                 "tier-b", 2,
+                                 "tier-a", 3)
+
+    def set_tier(self, name=None, order=10):
+        _log.debug("Setting tier data: \n"
+                   "name : %s\norder : %s",
+                   name, order)
+        if name is None:
+            name = self.policy_tier_name
+        tier = {"apiVersion": "v1",
+                "kind": "tier",
+                "metadata": {"name": name},
+                "spec": {"order": order}
+                }
+        self._apply_data(tier, self.hosts[0])
+
+    def _do_tier_order_test(self, first_tier, first_tier_order, second_tier,
+                            second_tier_order, third_tier, third_tier_order):
+        policy_next_all = {
+            "apiVersion": "v1",
+            "kind": "policy",
+            "metadata": {"name": "policy_next_all"},
+            "spec": {
+                "order": 10,
+                "ingress": [{"action": "next-tier"}],
+                "egress": [{"action": "next-tier"}]
+            }
+        }
+
+        policy_allow_all = {
+            "apiVersion": "v1",
+            "kind": "policy",
+            "metadata": {"name": "policy_allow_all"},
+            "spec": {
+                "order": 10,
+                "inbound_rules": [{"action": "allow"}],
+                "outbound_rules": [{"action": "allow"}]
+            }
+        }
+
+        policy_deny_all = {
+            "apiVersion": "v1",
+            "kind": "policy",
+            "metadata": {"name": "policy_deny_all"},
+            "spec": {
+                "order": 10,
+                "inbound_rules": [{"action": "deny"}],
+                "outbound_rules": [{"action": "deny"}]}
+        }
+
+        # Note that the following tests need to check that connectivity to
+        # the rpcbind service alternates between succeeding and failing so
+        # that we spot if felix hasn't actually changed anything.
+        # Check we start with connectivity.
+        _log.info("Starting tier order test %s (%s); %s (%s); %s (%s)",
+                  first_tier, first_tier_order, second_tier,
+                  second_tier_order, third_tier, third_tier_order)
+
+        self.assert_connectivity(self.n1_workloads)
+
+        # Create tiers and endpoints.
+        _log.info("Configuring tiers.")
+        self.set_tier(name=first_tier, order=first_tier_order)
+        self.set_tier(name=second_tier, order=second_tier_order)
+        self.set_tier(name=third_tier, order=third_tier_order)
+
+        # Slip a deny into the third tier, just to alternate DENY/ALLOW.
+        self.set_policy(third_tier, "pol-1", policy_deny_all)
+        # Check that access is blocked.
+        self.assert_no_connectivity(self.n1_workloads)
+
+        # Allow in first tier only, should allow.
+        _log.info("Allow in first tier only, should allow.")
+        self.set_policy(first_tier, "pol-1", policy_allow_all)
+        self.set_policy(second_tier, "pol-1", policy_deny_all)
+        self.set_policy(third_tier, "pol-1", policy_deny_all)
+        self.assert_connectivity(self.n1_workloads)
+
+        # Deny in all tiers, should drop.
+        _log.info("Deny in all tiers, should drop.")
+        # Fix up second tier
+        self.set_policy(second_tier, "pol-1", policy_deny_all)
+        self.set_policy(first_tier, "pol-1", policy_deny_all)
+        self.assert_no_connectivity(self.n1_workloads)
+
+        # Allow in first tier, should allow.
+        self.set_policy(first_tier, "pol-1", policy_allow_all)
+        self.assert_connectivity(self.n1_workloads)
+
+        # Switch, now the first tier drops but the later ones allow.
+        _log.info("Switch, now the first tier drops but the later ones "
+                  "allow.")
+        self.set_policy(first_tier, "pol-1", policy_deny_all)
+        self.set_policy(second_tier, "pol-1", policy_allow_all)
+        self.set_policy(third_tier, "pol-1", policy_allow_all)
+        self.assert_no_connectivity(self.n1_workloads)
+
+        # Fall through via a next-tier policy in the first tier.
+        _log.info("Fall through via a next-tier policy in the first "
+                  "tier.")
+        self.set_policy(first_tier, "pol-1", policy_next_all)
+        self.assert_connectivity(self.n1_workloads)
+
+        # Swap the second tier for a drop.
+        _log.info("Swap the second tier for a drop.")
+        self.set_policy(second_tier, "pol-1", policy_deny_all)
+        self.assert_no_connectivity(self.n1_workloads)
+
+    @staticmethod
+    def _apply_data(data, host):
+        _log.debug("Applying data with calicoctl: %s", data)
+        # Apply new profiles
+        host.writefile("new_profile",
+                       yaml.dump(data, default_flow_style=False))
+        host.calicoctl("apply -f new_profile")
+
+    def set_policy(self, tier, policy_name, data, order=None):
+        data = copy.deepcopy(data)
+        if order is not None:
+            data["order"] = order
+        data["metadata"]["name"] = policy_name
+        data["metadata"]["tier"] = tier
+
+        self._apply_data(data, self.hosts[0])
+
+    def assert_no_connectivity(self, workload_list, retries=0, type_list=None):
+        """
+        Checks that none of the workloads passed in can contact any of the others.
+        :param workload_list:
+        :param retries:
+        :param type_list:
+        :return:
+        """
+        for workload in workload_list:
+            the_rest = [wl for wl in workload_list if wl is not workload]
+            self.assert_connectivity([workload], fail_list=the_rest,
+                                     retries=retries, type_list=type_list)
+
+
+class IpNotFound(Exception):
+    pass
