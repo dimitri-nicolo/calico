15a9e028685195903510a95e209c6e3f97e12a7d
Karthik Ramasubramanian <karthik@tigera.io>
2017-08-17 16:49:53 -0700

    Split IPFIX tests into a separate file
    IPFIX tests will remain disabled until we need them

--- calico_node/tests/st/enterprise/test_tiered_policy.py
+++ calico_node/tests/st/enterprise/test_tiered_policy.py
@@ -1,35 +1,18 @@
-# Copyright 2016 Tigera, Inc
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2016-2017 Tigera, Inc
 import copy
 import functools
 import json
 import logging
-import netaddr
 import subprocess
 import time
-import unittest
 import yaml
 from nose_parameterized import parameterized
 from multiprocessing.dummy import Pool
 
 from tests.st.test_base import TestBase
-from tests.st.enterprise.utils.ipfix_monitor import IpfixFlow, IpfixMonitor
 from tests.st.utils.docker_host import DockerHost
-from tests.st.utils.exceptions import CommandExecError
-from tests.st.utils.utils import assert_network, assert_profile, \
-    assert_number_endpoints, get_profile_name, ETCD_CA, ETCD_CERT, \
-    ETCD_KEY, ETCD_HOSTNAME_SSL, ETCD_SCHEME, get_ip
+from tests.st.utils.utils import assert_number_endpoints, get_ip, \
+    ETCD_CA, ETCD_CERT, ETCD_KEY, ETCD_HOSTNAME_SSL, ETCD_SCHEME
 
 _log = logging.getLogger(__name__)
 _log.setLevel(logging.DEBUG)
@@ -105,167 +88,6 @@ def curl_etcd(ip, path, options=None, recursive=True):
     rc = subprocess.check_output(command, shell=True)
     return json.loads(rc.strip())
 
-
-class MultiHostIpfix(TestBase):
-    @classmethod
-    def setUpClass(cls):
-        super(TestBase, cls).setUpClass()
-        cls.hosts = []
-        cls.hosts.append(DockerHost("host1",
-                                    additional_docker_options=ADDITIONAL_DOCKER_OPTIONS,
-                                    post_docker_commands=POST_DOCKER_COMMANDS,
-                                    start_calico=False))
-        cls.hosts.append(DockerHost("host2",
-                                    additional_docker_options=ADDITIONAL_DOCKER_OPTIONS,
-                                    post_docker_commands=POST_DOCKER_COMMANDS,
-                                    start_calico=False))
-        for host in cls.hosts:
-            host.start_calico_node()
-
-        # Configure the address of the ipfix collector.
-        cls.hosts[0].calicoctl("config set IpfixCollectorAddr " + get_ip() + " --raw=felix")
-        # Disappointingly, tshark only appears to be able to decode IPFIX when the UDP port is 4739.
-        cls.hosts[0].calicoctl("config set IpfixCollectorPort 4739 --raw=felix")
-        time.sleep(10)
-
-        cls.networks = []
-        cls.networks.append(cls.hosts[0].create_network("testnet1"))
-
-        cls.n1_workloads = []
-        # Create two workloads on cls.hosts[0] and one on cls.hosts[1] all in network 1.
-        cls.n1_workloads.append(cls.hosts[1].create_workload("workload_h2n1_1",
-                                                             image="workload",
-                                                             network=cls.networks[0]))
-        cls.n1_workloads.append(cls.hosts[0].create_workload("workload_h1n1_1",
-                                                             image="workload",
-                                                             network=cls.networks[0]))
-        cls.n1_workloads.append(cls.hosts[0].create_workload("workload_h1n1_2",
-                                                             image="workload",
-                                                             network=cls.networks[0]))
-        # Assert that endpoints are in Calico
-        assert_number_endpoints(cls.hosts[0], 2)
-        assert_number_endpoints(cls.hosts[1], 1)
-
-    @classmethod
-    def tearDownClass(cls):
-        # Tidy up
-        for host in cls.hosts:
-            host.remove_workloads()
-        for network in cls.networks:
-            network.delete()
-        for host in cls.hosts:
-            host.cleanup()
-            del host
-
-    def setUp(self):
-        # Start monitoring flows.  When writing tests be aware that flows will continue
-        # to be reported for a short while after they the actual packets stop.
-        _log.debug("Create flow monitor for each test")
-        self.mon = IpfixMonitor(get_ip(), 4739)
-
-    def tearDown(self):
-        # Reset flows after every test
-        _log.debug("Deleting flow monitor after test")
-        del self.mon
-
-    @unittest.skip("Disabled until Matt updates IPFix monitor")
-    def test_multi_host_ping(self, iteration=1):
-        """
-        Run a mainline multi-host test with IPFIX.
-        Because multihost tests are slow to setup, this tests most mainline
-        functionality in a single test.
-        - Create two hosts
-        - Create a network using the Calico IPAM driver, and a workload on
-          each host assigned to that network.
-        - Check that hosts on the same network can ping each other.
-        - Check that hosts on different networks cannot ping each other.
-        - Modify the profile rules
-        - Check that connectivity has changed to match the profile we set up
-        - Re-apply the original profile
-        - Check that connectivity goes back to what it was originally.
-        """
-        _log.debug("*"*80)
-        _log.debug("Iteration %s", iteration)
-
-        # Send a single ping packet between two workloads, and ensure it's reported in the
-        # ipfix output. The flow is recorded at both ends, so it appears twice.
-        print "==== Checking ping reported by ipfix ===="
-        self.n1_workloads[0].check_can_ping(self.n1_workloads[1].ip, retries=0)
-        ping_flows = [IpfixFlow(self.n1_workloads[0].ip,
-                                self.n1_workloads[1].ip,
-                                packets="1,1",
-                                octets="84,84"),
-                      IpfixFlow(self.n1_workloads[1].ip,
-                                self.n1_workloads[0].ip,
-                                packets="1,1",
-                                octets="84,84")]
-        self.mon.assert_flows_present(ping_flows, 20, allow_others=False)
-
-    @unittest.skip("Disabled until Matt updates IPFix monitor")
-    def test_multi_host_tcp(self, iteration=1):
-        """
-        Run a mainline multi-host test with IPFIX.
-        Because multihost tests are slow to setup, this tests most mainline
-        functionality in a single test.
-        - Create two hosts
-        - Create a network using the Calico IPAM driver, and a workload on
-          each host assigned to that network.
-        - Check that hosts on the same network can ping each other.
-        - Check that hosts on different networks cannot ping each other.
-        - Modify the profile rules
-        - Check that connectivity has changed to match the profile we set up
-        - Re-apply the original profile
-        - Check that connectivity goes back to what it was originally.
-        """
-        _log.debug("*"*80)
-        _log.debug("Iteration %s", iteration)
-
-        # Send a single ping packet between two workloads, and ensure it's reported in the
-        # ipfix output. The flow is recorded at both ends, so it appears twice.
-        print "==== Checking tcp reported by ipfix ===="
-        self.n1_workloads[0].check_can_tcp(self.n1_workloads[1].ip, retries=0)
-        ping_flows = [IpfixFlow(self.n1_workloads[0].ip,
-                                self.n1_workloads[1].ip,
-                                packets="4,6",
-                                octets="222,326"),
-                      IpfixFlow(self.n1_workloads[1].ip,
-                                self.n1_workloads[0].ip,
-                                packets="6,4",
-                                octets="326,222")]
-        self.mon.assert_flows_present(ping_flows, 20, allow_others=False)
-
-    @unittest.skip("Disabled until Matt updates IPFix monitor")
-    def test_multi_host_udp(self, iteration=1):
-        """
-        Run a mainline multi-host test with IPFIX.
-        Because multihost tests are slow to setup, this tests most mainline
-        functionality in a single test.
-        - Create two hosts
-        - Create a network using the Calico IPAM driver, and a workload on
-          each host assigned to that network.
-        - Check that hosts on the same network can ping each other.
-        - Check that hosts on different networks cannot ping each other.
-        - Modify the profile rules
-        - Check that connectivity has changed to match the profile we set up
-        - Re-apply the original profile
-        - Check that connectivity goes back to what it was originally.
-        """
-        _log.debug("*"*80)
-        _log.debug("Iteration %s", iteration)
-
-        # Send a single ping packet between two workloads, and ensure it's reported in the
-        # ipfix output. The flow is recorded at both ends, so it appears twice.
-        print "==== Checking udp reported by ipfix ===="
-        self.n1_workloads[0].check_can_udp(self.n1_workloads[1].ip, retries=0)
-        ping_flows = [IpfixFlow(self.n1_workloads[0].ip,
-                                self.n1_workloads[1].ip,
-                                packets="1,1",
-                                octets="34,34"),
-                      IpfixFlow(self.n1_workloads[1].ip,
-                                self.n1_workloads[0].ip,
-                                packets="1,1",
-                                octets="34,34")]
-        self.mon.assert_flows_present(ping_flows, 20, allow_others=False)
 
 policy_next_all = {
     "apiVersion": "v1",
