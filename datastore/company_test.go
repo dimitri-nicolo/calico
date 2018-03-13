e870afe3169df840c41ff4848c82742788e7657c
Doug Collier <doug.collier@gmail.com>
2018-03-12 20:55:52 -0700

    use sql-mock for unit testing db ops

--- datastore/company_test.go
+++ datastore/company_test.go
@@ -1,36 +1,44 @@
 package datastore
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/onsi/gomega"
+	"github.com/DATA-DOG/go-sqlmock"
+	uuidgen "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
 )
 
-func TestAllCompanies(t *testing.T) {
-	t.Run("Test All Companies", func(t *testing.T) {
-		gomega.RegisterTestingT(t)
-		db, err := NewDB(fmt.Sprintf("%v:%v@/tigera_backoffice", "root", "r00tPa$$w0rd"))
-		defer tearDown()
+func TestInsertCompany(t *testing.T) {
+	t.Run("Test Company APIs", func(t *testing.T) {
+		const (
+			testKey = "TestCompanyKey"
+			testName = "Test Company Name"
+		)
+		dbm, mock, err := sqlmock.New()
 		if err != nil {
-			t.Errorf("unable to connect to the db for testing")
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 		}
-		c, err := db.CreateCompany(&Company{Key: "TestKey", Name: "Testing Company"})
+		defer dbm.Close()
+		db := &DB{dbm}
+
+		uuid := uuidgen.NewV4().String()
+		mock.ExpectExec("INSERT INTO companies").
+			WithArgs(uuid, testKey, testName).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+
+		c, err := db.CreateCompany(&Company{Uuid: uuid, Key: testKey, Name: testName})
 		if err != nil {
 			t.Errorf("error creating company")
 		}
-		c, err = db.GetCompanyById(c.Id)
-		if err != nil {
-			t.Errorf("error getting company by id")
-		}
-		c, err = db.GetCompanyByUuid(c.Uuid)
-		if err != nil {
-			t.Errorf("error getting company by uuid")
-		}
 
+		require.Equal(t, int64(1), c.Id)
+		require.Equal(t, uuid, c.Uuid)
+		require.Equal(t, testKey, c.Key)
+		require.Equal(t, testName, c.Name)
+
+		// we make sure that all expectations were met
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expectations: %s", err)
+		}
 	})
-}
-
-func tearDown() {
-
 }
