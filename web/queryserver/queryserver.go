a5c837d912dc6d8e6d5239558bce39dd91b8e7c9
matt <mleung975@gmail.com>
2018-03-21 16:38:34 -0700

    Add ability to generate self signed certs for TLS

--- web/queryserver/queryserver.go
+++ web/queryserver/queryserver.go
@@ -2,14 +2,22 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"os"
 
+	"github.com/golang/glog"
 	log "github.com/sirupsen/logrus"
+	certutil "k8s.io/client-go/util/cert"
 
 	"github.com/projectcalico/libcalico-go/lib/logutils"
 	"github.com/tigera/calicoq/web/pkg/clientmgr"
 	"github.com/tigera/calicoq/web/queryserver/server"
 )
+
+// Use the same certificates as the cnx-apiserver.
+const queryServerCertPath = "apiserver.local.config/certificates/apiserver.crt"
+const queryServerKeyPath = "apiserver.local.config/certificates/apiserver.key"
 
 func main() {
 	// Set the logging level (default to warning).
@@ -20,11 +28,6 @@ func main() {
 	}
 	log.SetLevel(logLevel)
 
-	// TODO: Make this a better check than just pulling variables
-	// Possibly switch this to use the golang TLSConfig if necessary.
-	webKey := os.Getenv("QUERYSERVER_KEY")
-	webCert := os.Getenv("QUERYSERVER_CERT")
-
 	// Load the client configuration.  Currently we only support loading from environment.
 	cfg, err := clientmgr.LoadClientConfig("")
 	if err != nil {
@@ -32,12 +35,44 @@ func main() {
 	}
 	log.Infof("Loaded client config: %#v", cfg.Spec)
 
-	if webKey != "" && webCert != "" {
-		server.Start(":10443", cfg, webCert, webKey)
-	} else {
-		server.Start(":8080", cfg, "", "")
+	// Generate self signed cert and key files for use with the kube proxy.
+	// Allowed to use self signed certs since the kube proxy will not be using
+	// the certificate for identity verification.
+	if err := MaybeDefaultWithSelfSignedCerts("localhost", nil, []net.IP{net.ParseIP("127.0.0.1")}); err != nil {
+		log.Errorf("Error creating self-signed certificates: %v", err)
+		os.Exit(1)
 	}
 
-	// Wait while the server is running
+	// Start the server
+	server.Start(":8080", cfg, queryServerKeyPath, queryServerCertPath)
+
+	// Wait while the server is running.
 	server.Wait()
+}
+
+// MaybeDefaultWithSelfSignedCerts is a minimal copy of the version in the kubernetes apiserver in order to avoid versioning issues.
+func MaybeDefaultWithSelfSignedCerts(publicAddress string, alternateDNS []string, alternateIPs []net.IP) error {
+	canReadCertAndKey, err := certutil.CanReadCertAndKey(queryServerCertPath, queryServerKeyPath)
+	if err != nil {
+		return err
+	}
+	if !canReadCertAndKey {
+		// add localhost to the valid alternates
+		alternateDNS = append(alternateDNS, "localhost")
+
+		if cert, key, err := certutil.GenerateSelfSignedCertKey(publicAddress, alternateIPs, alternateDNS); err != nil {
+			return fmt.Errorf("unable to generate self signed cert: %v", err)
+		} else {
+			if err := certutil.WriteCert(queryServerCertPath, cert); err != nil {
+				return err
+			}
+
+			if err := certutil.WriteKey(queryServerKeyPath, key); err != nil {
+				return err
+			}
+			glog.Infof("Generated self-signed cert (%s, %s)", queryServerCertPath, queryServerKeyPath)
+		}
+	}
+
+	return nil
 }
