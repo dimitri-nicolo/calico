6129e0f5c02789e10680b1d01fbb23cbe456331f
Karthik Ramasubramanian <karthik@tigera.io>
2018-07-12 14:56:48 -0700

    Add support for parsing conntrack TCP state protoinfo

--- conntrack_test.go
+++ conntrack_test.go
@@ -1,6 +1,7 @@
 package nfnetlink
 
 import (
+	"net"
 	"syscall"
 	"testing"
 
@@ -70,6 +71,61 @@ var _ = Describe("Conntrack Entry DNAT", func() {
 	})
 })
 
+var _ = Describe("Conntrack Entry Parsing", func() {
+	Context("TCP", func() {
+		data := [...]byte{52, 0, 1, 128, 20, 0, 1, 128, 8, 0, 1, 0, 10, 0, 2, 15, 8, 0, 2, 0, 216, 58, 217, 36, 28, 0, 2, 128, 5, 0, 1, 0, 6, 0, 0, 0, 6, 0, 2, 0, 236, 210, 0, 0, 6, 0, 3, 0, 0, 80, 0, 0, 52, 0, 2, 128, 20, 0, 1, 128, 8, 0, 1, 0, 216, 58, 217, 36, 8, 0, 2, 0, 10, 0, 2, 15, 28, 0, 2, 128, 5, 0, 1, 0, 6, 0, 0, 0, 6, 0, 2, 0, 0, 80, 0, 0, 6, 0, 3, 0, 236, 210, 0, 0, 8, 0, 3, 0, 0, 0, 1, 142, 8, 0, 7, 0, 0, 0, 0, 114, 28, 0, 9, 128, 12, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 9, 12, 0, 2, 0, 0, 0, 0, 0, 0, 0, 1, 202, 28, 0, 10, 128, 12, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 8, 12, 0, 2, 0, 0, 0, 0, 0, 0, 0, 46, 56, 48, 0, 4, 128, 44, 0, 1, 128, 5, 0, 1, 0, 7, 0, 0, 0, 5, 0, 2, 0, 0, 0, 0, 0, 5, 0, 3, 0, 0, 0, 0, 0, 6, 0, 4, 0, 39, 0, 0, 0, 6, 0, 5, 0, 32, 0, 0, 0, 8, 0, 8, 0, 0, 0, 0, 0, 8, 0, 12, 0, 182, 70, 136, 192, 8, 0, 11, 0, 0, 0, 0, 1}
+		original := CtTuple{
+			Src:        ipStrTo16Byte("10.0.2.15"),
+			Dst:        ipStrTo16Byte("216.58.217.36"),
+			L3ProtoNum: 2,
+			ProtoNum:   6,
+			L4Src: CtL4Src{
+				Port: 60626,
+			},
+			L4Dst: CtL4Dst{
+				Port: 80,
+			},
+		}
+		reply := CtTuple{
+			Src:        ipStrTo16Byte("216.58.217.36"),
+			Dst:        ipStrTo16Byte("10.0.2.15"),
+			L3ProtoNum: 2,
+			ProtoNum:   6,
+			L4Src: CtL4Src{
+				Port: 80,
+			},
+			L4Dst: CtL4Dst{
+				Port: 60626,
+			},
+		}
+		orig_counters := CtCounters{
+			Packets: 9,
+			Bytes:   458,
+		}
+		reply_counters := CtCounters{
+			Packets: 8,
+			Bytes:   11832,
+		}
+		It("should parse conntrack entry correctly", func() {
+			By("Parsing the byte array")
+			ctentry, err := conntrackEntryFromNfAttrs(data[:], syscall.AF_INET)
+			Expect(err).To(BeNil())
+
+			By("checking the parsed conntrack is not NAT-ted")
+			Expect(ctentry.IsDNAT()).To(Equal(false))
+			Expect(ctentry.IsNAT()).To(Equal(false))
+			Expect(ctentry.IsSNAT()).To(Equal(false))
+
+			By("checking the fields of conntrack entry")
+			Expect(ctentry.OriginalTuple).To(Equal(original))
+			Expect(ctentry.ReplyTuple).To(Equal(reply))
+			Expect(ctentry.OriginalCounters).To(Equal(orig_counters))
+			Expect(ctentry.ReplyCounters).To(Equal(reply_counters))
+			Expect(ctentry.ProtoInfo.State).To(Equal(nfnl.TCP_CONNTRACK_TIME_WAIT))
+		})
+	})
+})
+
 func BenchmarkConntrackEntryFromNfAttrs(b *testing.B) {
 	// Setup
 	data := [...]byte{52, 0, 1, 128, 20, 0, 1, 128, 8, 0, 1, 0, 10, 0, 2, 2, 8, 0, 2, 0, 10, 0, 2, 15, 28, 0, 2, 128, 5, 0, 1, 0, 6, 0, 0, 0, 6, 0, 2, 0, 207, 206, 0, 0, 6, 0, 3, 0, 0, 22, 0, 0, 52, 0, 2, 128, 20, 0, 1, 128, 8, 0, 1, 0, 10, 0, 2, 15, 8, 0, 2, 0, 10, 0, 2, 2, 28, 0, 2, 128, 5, 0, 1, 0, 6, 0, 0, 0, 6, 0, 2, 0, 0, 22, 0, 0, 6, 0, 3, 0, 207, 206, 0, 0, 8, 0, 3, 0, 0, 0, 1, 142, 8, 0, 7, 0, 0, 6, 145, 147, 48, 0, 4, 128, 44, 0, 1, 128, 5, 0, 1, 0, 3, 0, 0, 0, 5, 0, 2, 0, 0, 0, 0, 0, 5, 0, 3, 0, 0, 0, 0, 0, 6, 0, 4, 0, 32, 0, 0, 0, 6, 0, 5, 0, 32, 0, 0, 0, 8, 0, 8, 0, 0, 0, 0, 0, 8, 0, 12, 0, 186, 125, 96, 0, 8, 0, 11, 0, 0, 0, 0, 1}
@@ -81,4 +137,11 @@ func BenchmarkConntrackEntryFromNfAttrs(b *testing.B) {
 		conntrackEntryFromNfAttrs(data[:], syscall.AF_INET)
 
 	}
+}
+
+func ipStrTo16Byte(ipStr string) [16]byte {
+	addr := net.ParseIP(ipStr)
+	var addrB [16]byte
+	copy(addrB[:], addr.To16()[:16])
+	return addrB
 }
