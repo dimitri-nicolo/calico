473a4b0a7106b19c991436957ba8171c4ffa9e10
Karthik Ramasubramanian <karthik@tigera.io>
2018-07-23 14:34:58 -0700

    Revert custom retry and use on AWS SDK for retries

--- collector/cloudwatch_dispatcher.go
+++ collector/cloudwatch_dispatcher.go
@@ -22,13 +22,8 @@ const (
 	FlowLogFormatJSON FlowLogFormat = "json"
 	FlowLogFormatFlat FlowLogFormat = "flat"
 
-	cwAPITimeout        = 3 * time.Second
-	cwRetryWaitInterval = 1 * time.Second
-	cwNumRetries        = 5
-)
-
-var (
-	cwResourceNotFound = errors.New("couldn't find CloudWatch resource.")
+	cwAPITimeout = 5 * time.Second
+	cwNumRetries = 3
 )
 
 // cloudWatchDispatcher implements the FlowLogDispatcher interface.
@@ -51,7 +46,7 @@ func NewCloudWatchDispatcher(
 		sess := session.Must(session.NewSessionWithOptions(session.Options{
 			SharedConfigState: session.SharedConfigEnable,
 		}))
-		cwl = cloudwatchlogs.New(sess)
+		cwl = cloudwatchlogs.New(sess, aws.NewConfig().WithMaxRetries(cwNumRetries))
 	}
 
 	cwd := &cloudWatchDispatcher{
@@ -87,25 +82,22 @@ func (c *cloudWatchDispatcher) Dispatch(ctx context.Context, inputLogs []*string
 	if c.seqToken != "" {
 		params.SequenceToken = aws.String(c.seqToken)
 	}
-	putLogEventsFunc := func(ctx context.Context, inp interface{}) error {
-		params := inp.(*cloudwatchlogs.PutLogEventsInput)
-		ctx, cancel := context.WithTimeout(ctx, cwAPITimeout)
-		defer cancel()
-		resp, err := c.cwl.PutLogEventsWithContext(ctx, params)
-		if err != nil {
-			log.WithFields(log.Fields{"LogGroupName": c.logGroupName, "LogStreamName": c.logStreamName}).WithError(err).Error("PutLogevents")
-			return err
-		}
-		if resp.RejectedLogEventsInfo != nil {
-			log.Warnf("expired log event end index: %d", resp.RejectedLogEventsInfo.ExpiredLogEventEndIndex)
-			log.Warnf("too new log event start index: %d", resp.RejectedLogEventsInfo.TooNewLogEventStartIndex)
-			log.Warnf("too old log event end index: %d", resp.RejectedLogEventsInfo.TooOldLogEventEndIndex)
-			return errors.New("Got rejected put log events")
-		}
-		c.seqToken = *resp.NextSequenceToken
-		return nil
+	ctx, cancel := context.WithTimeout(ctx, cwAPITimeout)
+	defer cancel()
+	resp, err := c.cwl.PutLogEventsWithContext(ctx, params)
+
+	if err != nil {
+		log.WithFields(log.Fields{"LogGroupName": c.logGroupName, "LogStreamName": c.logStreamName}).WithError(err).Error("PutLogevents")
+		return err
 	}
-	return retryUntil(ctx, putLogEventsFunc, nil, cwNumRetries, params)
+	if resp.RejectedLogEventsInfo != nil {
+		log.Warnf("expired log event end index: %d", resp.RejectedLogEventsInfo.ExpiredLogEventEndIndex)
+		log.Warnf("too new log event start index: %d", resp.RejectedLogEventsInfo.TooNewLogEventStartIndex)
+		log.Warnf("too old log event end index: %d", resp.RejectedLogEventsInfo.TooOldLogEventEndIndex)
+		return errors.New("Got rejected put log events")
+	}
+	c.seqToken = *resp.NextSequenceToken
+	return nil
 }
 
 func (c *cloudWatchDispatcher) Initialize(ctx context.Context) error {
@@ -151,27 +143,16 @@ func (c *cloudWatchDispatcher) verifyOrCreateLogStream(ctx context.Context) erro
 		return err
 	}
 	log.WithField("LogStreamName", c.logStreamName).Info("Creating Log stream")
-	createLSFunc := func(ctx context.Context, inp interface{}) error {
-		createLSInp := inp.(*cloudwatchlogs.CreateLogStreamInput)
-		ctx, cancel := context.WithTimeout(ctx, cwAPITimeout)
-		defer cancel()
-		_, err = c.cwl.CreateLogStreamWithContext(ctx, createLSInp)
-		return err
-	}
-	onErr := func(err error) bool {
-		if isAWSError(err, cloudwatchlogs.ErrCodeResourceAlreadyExistsException) {
-			// LogStream exists already. This cannot happen unless someone manually
-			// created the log stream between us verifying if it exists (above) to
-			// trying to create it (here).
-			log.Debug("Log stream now exists")
-			return false
-		}
-		return true
-	}
-	err = retryUntil(ctx, createLSFunc, onErr, cwNumRetries, createLSInp)
-	if err != nil {
-		log.WithError(err).Error("Error when CreateLogStream")
-		return err
+	ctx, cancel := context.WithTimeout(ctx, cwAPITimeout)
+	defer cancel()
+	_, err = c.cwl.CreateLogStreamWithContext(ctx, createLSInp)
+	return err
+	if isAWSError(err, cloudwatchlogs.ErrCodeResourceAlreadyExistsException) {
+		// LogStream exists already. This cannot happen unless someone manually
+		// created the log stream between us verifying if it exists (above) to
+		// trying to create it (here).
+		log.Debug("Log stream now exists")
+		return nil
 	}
 
 	err = c.verifyLogStream(ctx)
@@ -192,36 +173,23 @@ func (c *cloudWatchDispatcher) verifyLogStream(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	descLogStreamFunc := func(ctx context.Context, inp interface{}) error {
-		descLSInp := inp.(*cloudwatchlogs.DescribeLogStreamsInput)
-		ctx, cancel := context.WithTimeout(ctx, cwAPITimeout)
-		defer cancel()
-		resp, err := c.cwl.DescribeLogStreamsWithContext(ctx, descLSInp)
-		if err != nil {
-			log.WithError(err).Error("Error when DescribeLogStreams")
-			return err
-		}
-		log.Debugf(resp.GoString())
-		for _, ls := range resp.LogStreams {
-			log.Debugf(ls.GoString())
-			if *ls.LogStreamName == c.logStreamName {
-				c.setSeqToken(ls)
-				return nil
-			}
-		}
-		return cwResourceNotFound
+	ctx, cancel := context.WithTimeout(ctx, cwAPITimeout)
+	defer cancel()
+	resp, err := c.cwl.DescribeLogStreamsWithContext(ctx, descLSInp)
+	if err != nil {
+		log.WithError(err).Error("Error when DescribeLogStreams")
+		return err
 	}
-	onErr := func(err error) bool {
-		if isAWSError(err, cloudwatchlogs.ErrCodeResourceNotFoundException) || err == cwResourceNotFound {
-			// LogStream does not exist. We can stop retrying and return the error.
-			log.WithFields(log.Fields{"LogGroupName": c.logGroupName, "LogStreamName": c.logStreamName}).Debug("LogStream does not exist")
-			return false
+	log.Debugf(resp.GoString())
+	for _, ls := range resp.LogStreams {
+		log.Debugf(ls.GoString())
+		if *ls.LogStreamName == c.logStreamName {
+			c.setSeqToken(ls)
+			return nil
 		}
-		// For all other errors, try to retry.
-		log.WithFields(log.Fields{"LogGroupName": c.logGroupName, "LogStreamName": c.logStreamName}).WithError(err).Error("Error when DescribeLogStreams")
-		return true
 	}
-	return retryUntil(ctx, descLogStreamFunc, onErr, cwNumRetries, descLSInp)
+	return fmt.Errorf("Cannot find log stream %v in log group %v", c.logStreamName, c.logGroupName)
+
 }
 
 func (c *cloudWatchDispatcher) setLogGroupRetention(ctx context.Context) error {
@@ -235,18 +203,10 @@ func (c *cloudWatchDispatcher) setLogGroupRetention(ctx context.Context) error {
 		log.WithError(err).Warning("Invalid input for PutRetentionPolicy call")
 		return err
 	}
-	putRetPolFunc := func(ctx context.Context, inp interface{}) error {
-		putRetentionInp := inp.(*cloudwatchlogs.PutRetentionPolicyInput)
-		ctx, cancel := context.WithTimeout(ctx, cwAPITimeout)
-		defer cancel()
-		_, err = c.cwl.PutRetentionPolicyWithContext(ctx, putRetentionInp)
-		if err != nil {
-			log.WithError(err).Warning("Error in PutRetentionPolicy call")
-			return err
-		}
-		return nil
-	}
-	return retryUntil(ctx, putRetPolFunc, nil, cwNumRetries, putRetentionInp)
+	ctx, cancel := context.WithTimeout(ctx, cwAPITimeout)
+	defer cancel()
+	_, err = c.cwl.PutRetentionPolicyWithContext(ctx, putRetentionInp)
+	return err
 }
 
 func (c *cloudWatchDispatcher) verifyOrCreateLogGroup(ctx context.Context) error {
@@ -265,31 +225,27 @@ func (c *cloudWatchDispatcher) verifyOrCreateLogGroup(ctx context.Context) error
 		return err
 	}
 	log.WithField("LogGroupName", c.logGroupName).Info("Creating Log group")
-	createLGFunc := func(ctx context.Context, inp interface{}) error {
-		createLGInp := inp.(*cloudwatchlogs.CreateLogGroupInput)
-		ctx, cancel := context.WithTimeout(ctx, cwAPITimeout)
-		defer cancel()
-		_, err = c.cwl.CreateLogGroupWithContext(ctx, createLGInp)
-		if err == nil {
-			// LogGroup just created by us; set its retention time.
-			err = c.setLogGroupRetention(ctx)
-			if err != nil {
-				return err
-			}
-		} else if isAWSError(err, cloudwatchlogs.ErrCodeResourceAlreadyExistsException) {
-			// LogGroup just created by another ANX node.  Don't set its retention
-			// time; there's no need for more than one node to do this, and we can
-			// assume that the other node has (or will) set its retention time to
-			// whatever the current FelixConfiguration setting says.
-			log.Debug("Log group now exists; presume just created by another ANX node")
-		} else {
-			// Some error other than a creation race.
-			log.WithField("LogGroupName", c.logGroupName).WithError(err).Error("Error creating Log group")
+	ctx, cancel := context.WithTimeout(ctx, cwAPITimeout)
+	defer cancel()
+	_, err = c.cwl.CreateLogGroupWithContext(ctx, createLGInp)
+	if err == nil {
+		// LogGroup just created by us; set its retention time.
+		err = c.setLogGroupRetention(ctx)
+		if err != nil {
 			return err
 		}
-		return nil
+	} else if isAWSError(err, cloudwatchlogs.ErrCodeResourceAlreadyExistsException) {
+		// LogGroup just created by another ANX node.  Don't set its retention
+		// time; there's no need for more than one node to do this, and we can
+		// assume that the other node has (or will) set its retention time to
+		// whatever the current FelixConfiguration setting says.
+		log.Debug("Log group now exists; presume just created by another ANX node")
+	} else {
+		// Some error other than a creation race.
+		log.WithField("LogGroupName", c.logGroupName).WithError(err).Error("Error creating Log group")
+		return err
 	}
-	return retryUntil(ctx, createLGFunc, nil, cwNumRetries, createLGInp)
+	return nil
 }
 
 func isAWSError(err error, codes ...string) bool {
@@ -315,81 +271,31 @@ func (c *cloudWatchDispatcher) verifyLogGroup(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	descLogGroupFunc := func(ctx context.Context, inp interface{}) error {
-		descLGInp := inp.(*cloudwatchlogs.DescribeLogGroupsInput)
-		ctx, cancel := context.WithTimeout(ctx, cwAPITimeout)
-		defer cancel()
-		log.Debugf("Describe %v\n", c.logGroupName)
-		resp, err := c.cwl.DescribeLogGroupsWithContext(ctx, descLGInp)
-		if err != nil {
-			return err
-		}
-		log.Debugf(resp.GoString())
-		for _, lg := range resp.LogGroups {
-			log.Debugf(lg.GoString())
-			if *lg.LogGroupName == c.logGroupName {
-				log.Debugf("Found log group %v", c.logGroupName)
-				if lg.RetentionInDays == nil || *lg.RetentionInDays != int64(c.retentionDays) {
-					// Log group's retention period does not match the current
-					// FelixConfiguration setting, so try to change it to
-					// match.  If there is an error here,
-					// setLogGroupRetention() will log it, but we don't
-					// propagate it any further upwards from this point.  The
-					// next ANX node that starts up will try again to align
-					// the period with FelixConfiguration.
-					c.setLogGroupRetention(ctx)
-				}
-				return nil
-			}
-		}
-		return cwResourceNotFound
-	}
-	onErr := func(err error) bool {
-		if isAWSError(err, cloudwatchlogs.ErrCodeResourceNotFoundException) || err == cwResourceNotFound {
-			// LogGroup does not exist. We can stop retrying and return the error.
-			log.WithField("LogGroupName", c.logGroupName).Debug("Log group does not exists")
-			return false
-		}
-		return true
-	}
-	return retryUntil(ctx, descLogGroupFunc, onErr, cwNumRetries, descLGInp)
-}
-
-type retryableFunc func(context.Context, interface{}) error
-
-type shouldRetryOnError func(error) bool
-
-type retriesExceeded struct {
-	origError error
-}
-
-func (e *retriesExceeded) Error() string {
-	return fmt.Sprintf("Retries exceeded. Last recorded error %v", e.origError)
-}
-
-func retryUntil(ctx context.Context, fn retryableFunc, shouldRetry shouldRetryOnError, maxRetries int, input interface{}) error {
-	var err error
-	// Initial attempt before we start retrying
-	err = fn(ctx, input)
-	if err == nil || (shouldRetry != nil && !shouldRetry(err)) {
+	ctx, cancel := context.WithTimeout(ctx, cwAPITimeout)
+	defer cancel()
+	log.Debugf("Describe %v\n", c.logGroupName)
+	resp, err := c.cwl.DescribeLogGroupsWithContext(ctx, descLGInp)
+	if err != nil {
+		log.WithError(err).Errorf("Describe error %v\n", c.logGroupName)
 		return err
 	}
-	backoffChan := time.NewTicker(cwRetryWaitInterval).C
-	attempt := 1
-	for {
-		select {
-		case <-backoffChan:
-			err = fn(ctx, input)
-			if err == nil || (shouldRetry != nil && !shouldRetry(err)) {
-				break
+	log.Debugf(resp.GoString())
+	for _, lg := range resp.LogGroups {
+		log.Debugf(lg.GoString())
+		if *lg.LogGroupName == c.logGroupName {
+			log.Debugf("Found log group %v", c.logGroupName)
+			if lg.RetentionInDays == nil || *lg.RetentionInDays != int64(c.retentionDays) {
+				// Log group's retention period does not match the current
+				// FelixConfiguration setting, so try to change it to
+				// match.  If there is an error here,
+				// setLogGroupRetention() will log it, but we don't
+				// propagate it any further upwards from this point.  The
+				// next ANX node that starts up will try again to align
+				// the period with FelixConfiguration.
+				c.setLogGroupRetention(ctx)
 			}
-			attempt++
-			if attempt >= maxRetries {
-				return &retriesExceeded{origError: err}
-			}
-			log.WithError(err).Debugf("Retrying %v", fn)
+			return nil
 		}
 	}
-
-	return err
+	return fmt.Errorf("Could not find log group %v", c.logGroupName)
 }
