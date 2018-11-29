41ecb2f1c2bff333842274cd6ea7d046fb0899c2
Shaun Crampton <shaun@tigera.io>
2018-11-29 15:53:35 +0000

    Clean up confd-generated files when starting confd service on Windows.
    Ensures we always run the reconfig script at start-of-day.

--- windows-packaging/TigeraCalico/confd/confd-service.ps1
+++ windows-packaging/TigeraCalico/confd/confd-service.ps1
@@ -10,8 +10,16 @@ if($env:CALICO_NETWORKING_BACKEND = "windows-bgp")
   Write-Host "Waiting for the first HNS network to be initialised..."
   Wait-ForManagementIP "External"
   Write-Host "Windows BGP is enabled, running confd..."
-  # Run the tigera-confd binary.
+
   cd "$PSScriptRoot"
+
+  # Remove the old peerings and blocks so that confd will always trigger
+  # reconfiguration at start of day.  This ensures that stopping and starting the service
+  # reliably recovers from previous failures.
+  rm peerings.ps1 -ErrorAction SilentlyContinue
+  rm blocks.ps1 -ErrorAction SilentlyContinue
+
+  # Run the tigera-confd binary.
   & ..\tigera-calico.exe -confd -confd-confdir="$PSScriptRoot"
 } else {
   Write-Host "Windows BGP is disabled, not running confd."
