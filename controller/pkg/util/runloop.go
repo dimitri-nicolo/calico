944c0949aba7948ee229b7c3dcfbed1c02c22109
Henry Stern <henry@tigera.io>
2019-03-07 11:46:14 -0400

    Add RunLoopWithReschedule function

--- controller/pkg/util/runloop.go
+++ controller/pkg/util/runloop.go
@@ -2,18 +2,82 @@ package util
 
 import (
 	"context"
+	"errors"
+	"sync"
 	"time"
 )
 
-func RunLoop(ctx context.Context, f func(), period time.Duration) {
+func RunLoop(ctx context.Context, f func(), period time.Duration) error {
+	return runLoop(ctx, func() {}, f, period, make(chan struct{}), func() {}, 0)
+}
+
+type RunFuncWithReschedule func(context.Context, func(), time.Duration, func(), time.Duration) error
+type RescheduleFunc func() error
+
+func RunLoopWithReschedule() (RunFuncWithReschedule, RescheduleFunc) {
+	ch := make(chan struct{})
+	var started bool
+
+	initFunc := func() {
+		started = true
+	}
+	runFunc := func(ctx context.Context, f func(), period time.Duration, rescheduleFunc func(), reschedulePeriod time.Duration) error {
+		return runLoop(ctx, initFunc, f, period, ch, rescheduleFunc, reschedulePeriod)
+	}
+	rescheduleFunc := func() (err error) {
+		if !started {
+			return errors.New("RunFunc has not yet started")
+		}
+		defer func() {
+			if r := recover(); r != nil {
+				err = errors.New("RunFunc has terminated")
+			}
+		}()
+		select {
+		case ch <- struct{}{}:
+			return nil
+		default:
+			return errors.New("RunFunc is not currently reschedulable")
+		}
+	}
+
+	return runFunc, rescheduleFunc
+}
+
+func runLoop(ctx context.Context, initFunc func(), f func(), period time.Duration, rescheduleCh chan struct{}, rescheduleFunc func(), reschedulePeriod time.Duration) error {
 	t := time.NewTicker(period)
 	defer t.Stop()
+	defer close(rescheduleCh)
 
+	initFunc()
+	wg := sync.WaitGroup{}
 	for {
-		f()
+		wg.Wait()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			f()
+		}()
 		select {
 		case <-ctx.Done():
-			return
+			return ctx.Err()
+		case <-rescheduleCh:
+			rescheduleFunc()
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-time.After(reschedulePeriod):
+				// drain t.C so that we don't run again immediately
+				for done := false; !done; {
+					select {
+					case <-t.C:
+						// nothing
+					default:
+						done = true
+					}
+				}
+				// continue
+			}
 		case <-t.C:
 			// continue
 		}
--- controller/pkg/util/runloop_test.go
+++ controller/pkg/util/runloop_test.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"context"
+	"sync"
 	"testing"
 	"time"
 
@@ -18,7 +19,152 @@ func TestRunLoop(t *testing.T) {
 	defer cancel()
 
 	c := 0
-	RunLoop(ctx, func() { c++ }, period)
+	var err error
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	cond := sync.Cond{
+		L: &sync.Mutex{},
+	}
+
+	go func() {
+		defer wg.Done()
+		err = RunLoop(ctx, func() {
+			cond.L.Lock()
+			cond.Signal()
+			cond.L.Unlock()
+			c++
+		}, period)
+	}()
+
+	// Measure the difference in time between executions
+	cond.L.Lock()
+	cond.Wait()
+	t0 := time.Now()
+	cond.L.Unlock()
+
+	cond.L.Lock()
+	cond.Wait()
+	t1 := time.Now()
+	cond.L.Unlock()
+
+	g.Expect(t1.Sub(t0)).Should(BeNumerically(">=", period))
+
+	wg.Wait()
+	g.Expect(err).Should(Equal(context.DeadlineExceeded))
 
 	g.Expect(c).Should(BeNumerically("~", maxDuration/period, 1))
+}
+
+func TestRunLoopWithReschedule(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	maxDuration := time.Millisecond * 10
+	period := 100 * time.Microsecond
+	reschedulePeriod := 10 * time.Microsecond
+
+	ctx, cancel := context.WithTimeout(context.TODO(), maxDuration)
+	defer cancel()
+
+	run, reschedule := RunLoopWithReschedule()
+	g.Expect(reschedule()).Should(HaveOccurred(), "Reschedule function should return an error if RunLoop has not started yet")
+
+	c := 0
+	rc := 0
+	var res error
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	cond := sync.Cond{
+		L: &sync.Mutex{},
+	}
+
+	go func() {
+		defer wg.Done()
+		res = run(ctx, func() {
+			cond.L.Lock()
+			cond.Signal()
+			cond.L.Unlock()
+			c++
+		}, period, func() { rc++ }, reschedulePeriod)
+	}()
+
+	// Measure the difference in time between executions when reschedule() is called
+	cond.L.Lock()
+	cond.Wait()
+	t0 := time.Now()
+	g.Expect(reschedule()).ShouldNot(HaveOccurred(), "Reschedule runs successfully")
+	g.Expect(reschedule()).Should(HaveOccurred(), "Reschedule fails")
+	cond.L.Unlock()
+
+	cond.L.Lock()
+	cond.Wait()
+	t1 := time.Now()
+	// Call reschedule again now that the reschedule has been cleared
+	g.Expect(reschedule()).ShouldNot(HaveOccurred(), "Reschedule runs successfully")
+	cond.L.Unlock()
+
+	g.Expect(t1.Sub(t0)).Should(BeNumerically("<", period))
+
+	wg.Wait()
+	g.Expect(res).Should(Equal(context.DeadlineExceeded))
+	g.Expect(c).Should(BeNumerically("~", maxDuration/period, 1+rc))
+
+	g.Expect(reschedule()).Should(HaveOccurred(), "Reschedule function returns an error after the RunLoop terminates and does not panic")
+	g.Expect(rc).Should(Equal(2))
+}
+
+func TestRunLoopWithRescheduleLongRunningFunction(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	maxDuration := time.Millisecond * 10
+	period := 100 * time.Microsecond
+	reschedulePeriod := 10 * time.Microsecond
+	sleep := time.Millisecond
+
+	ctx, cancel := context.WithTimeout(context.TODO(), maxDuration)
+	defer cancel()
+
+	run, reschedule := RunLoopWithReschedule()
+	g.Expect(reschedule()).Should(HaveOccurred(), "Reschedule function should return an error if RunLoop has not started yet")
+
+	c := 0
+	rc := 0
+	var res error
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	cond := sync.Cond{
+		L: &sync.Mutex{},
+	}
+
+	go func() {
+		defer wg.Done()
+		res = run(ctx, func() {
+			cond.L.Lock()
+			cond.Signal()
+			cond.L.Unlock()
+			c++
+			select {
+			case <-ctx.Done():
+			case <-time.After(sleep):
+			}
+		}, period, func() { rc++ }, reschedulePeriod)
+	}()
+
+	// Make sure that we can reschedule once while the long-running function is executing, but not twice
+	cond.L.Lock()
+	cond.Wait()
+	g.Expect(reschedule()).ShouldNot(HaveOccurred(), "Reschedule fails")
+	cond.L.Unlock()
+
+	cond.L.Lock()
+	cond.Wait()
+	g.Expect(reschedule()).Should(HaveOccurred(), "Reschedule fails")
+	cond.L.Unlock()
+
+	wg.Wait()
+	g.Expect(res).Should(Equal(context.DeadlineExceeded))
+
+	g.Expect(c).Should(BeNumerically("~", maxDuration/sleep, 1+rc))
 }
