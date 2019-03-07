93f66264b44c0cc999796da32cecca691689f195
Henry Stern <henry@tigera.io>
2019-03-07 15:52:20 -0400

    Update mappings

--- controller/pkg/elastic/mappings.go
+++ controller/pkg/elastic/mappings.go
@@ -16,107 +16,70 @@ const eventMapping = `{
   "mappings": {
     "_doc": {
       "properties" : {
-        "start_time": {
+        "time": {
             "type": "date",
             "format": "epoch_second"
         },
-        "end_time": {
-            "type": "date",
-            "format": "epoch_second"
-        },
-        "action": {
+		"type": {
             "type": "keyword"
         },
-        "bytes_in": {
-            "type": "long"
-        },
-        "bytes_out": {
-            "type": "long"
-        },
-        "dest_ip": {
-            "type": "ip",
-            "null_value": "0.0.0.0"
-        },
-        "dest_name": {
+		"description": {
+			"type": "keyword"
+		},
+		"severity": {
+			"type": "long"
+		},
+        "flow_log_index": {
             "type": "keyword"
         },
-        "dest_name_aggr": {
+        "flow_log_id": {
             "type": "keyword"
         },
-        "dest_namespace": {
+        "protocol": {
             "type": "keyword"
-        },
-        "dest_port": {
-            "type": "long",
-            "null_value": "0"
-        },
-        "dest_type": {
-            "type": "keyword"
-        },
-        "dest_labels": {
-                /* This is an array of keywords. It is not necessary to declare this as an array. Elastic will automatically accept a list of strings here */
-                "type": "nested",
-                "properties": {
-                        "labels": {"type": "keyword"}
-                }
-        },
-        "reporter": {
-            "type": "keyword"
-        },
-        "num_flows": {
-            "type": "long"
-        },
-        "num_flows_completed": {
-            "type": "long"
-        },
-        "num_flows_started": {
-            "type": "long"
-        },
-        "packets_in": {
-            "type": "long"
-        },
-        "packets_out": {
-            "type": "long"
-        },
-        "proto": {
-            "type": "keyword"
-        },
-        "policies": {
-                /* This is an array of keywords. It is not necessary to declare this as an array. Elastic will automatically accept a list of strings here */
-                "type": "nested",
-                "properties": {
-                        "all_policies": {"type": "keyword"}
-                }
         },
         "source_ip": {
             "type": "ip",
             "null_value": "0.0.0.0"
         },
-        "source_name": {
-            "type": "keyword"
-        },
-        "source_name_aggr": {
-            "type": "keyword"
-        },
-        "source_namespace": {
-            "type": "keyword"
-        },
         "source_port": {
             "type": "long",
             "null_value": "0"
         },
-        "source_type": {
+        "source_namespace": {
             "type": "keyword"
         },
-        "source_labels": {
+        "source_name": {
+            "type": "keyword"
+        },
+        "dest_ip": {
+            "type": "ip",
+            "null_value": "0.0.0.0"
+        },
+        "dest_port": {
+            "type": "long",
+            "null_value": "0"
+        },
+        "dest_namespace": {
+            "type": "keyword"
+        },
+        "dest_name": {
+            "type": "keyword"
+        },
+        "flow_action": {
+            "type": "keyword"
+        },
+        "feeds": {
                 /* This is an array of keywords. It is not necessary to declare this as an array. Elastic will automatically accept a list of strings here */
                 "type": "nested",
                 "properties": {
                         "labels": {"type": "keyword"}
                 }
-        }
+        },
+        "suspicious_prefix": {
+            "type": "keyword"
+        },
       }   
     }
   }
 }`
-
