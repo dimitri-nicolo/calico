7fe15c7ec16e343cf7c7cbb6533914b04ef39e8d
Karthik Ramasubramanian <karthik@tigera.io>
2019-03-31 20:41:00 -0700

    Update some proxy UTs

--- pkg/handler/proxy_test.go
+++ pkg/handler/proxy_test.go
@@ -15,25 +15,71 @@ var _ = Describe("Proxy Handler", func() {
 	targetName := "target"
 	var server, target *httptest.Server
 
-	BeforeEach(func() {
-		target = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Add("X-Target-Name", targetName)
-			w.Write([]byte(targetName))
-			w.WriteHeader(200)
-		}))
-		targetURL, err := url.Parse(target.URL)
-		Expect(err).ShouldNot(HaveOccurred())
-		server = httptest.NewServer(handler.NewProxy(targetURL))
+	Context("Proxy should proxy a response OK", func() {
+		BeforeEach(func() {
+			target = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(200)
+				w.Header().Add("X-Target-Name", targetName)
+				w.Write([]byte(targetName))
+			}))
+			targetURL, err := url.Parse(target.URL)
+			Expect(err).ShouldNot(HaveOccurred())
+			server = httptest.NewServer(handler.NewProxy(targetURL))
+		})
+		AfterEach(func() {
+			server.Close()
+			target.Close()
+		})
+		It("should forward requests to the target server", func() {
+			client := server.Client()
+			resp, err := client.Get(server.URL)
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(resp.StatusCode).Should(Equal(200))
+			Expect(resp.Header.Get("X-Target-Name")).Should(Equal(targetName))
+		})
 	})
-	AfterEach(func() {
-		server.Close()
-		target.Close()
+	Context("Status code must be sent unchanged by the proxy", func() {
+		BeforeEach(func() {
+			target = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(400)
+				w.Header().Add("X-Target-Name", targetName)
+			}))
+			targetURL, err := url.Parse(target.URL)
+			Expect(err).ShouldNot(HaveOccurred())
+			server = httptest.NewServer(handler.NewProxy(targetURL))
+		})
+		AfterEach(func() {
+			server.Close()
+			target.Close()
+		})
+		It("should not change the response code", func() {
+			client := server.Client()
+			resp, err := client.Get(server.URL)
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(resp.StatusCode).Should(Equal(400))
+			Expect(resp.Header.Get("X-Target-Name")).Should(Equal(targetName))
+		})
 	})
-	It("should forward requests to the target server", func() {
-		client := server.Client()
-		resp, err := client.Get(server.URL)
-		Expect(err).ShouldNot(HaveOccurred())
-		Expect(resp.StatusCode).Should(Equal(200))
-		Expect(resp.Header.Get("X-Target-Name")).Should(Equal(targetName))
+	Context("Proxy should timeout the request when the backend doesn't exist", func() {
+		BeforeEach(func() {
+			target = httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Add("X-Target-Name", targetName)
+				w.WriteHeader(200)
+			}))
+			targetURL, err := url.Parse(target.URL)
+			Expect(err).ShouldNot(HaveOccurred())
+			server = httptest.NewServer(handler.NewProxy(targetURL))
+		})
+		AfterEach(func() {
+			server.Close()
+			target.Close()
+		})
+		It("should respond with 502 bad gateway", func() {
+			client := server.Client()
+			resp, err := client.Get(server.URL)
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(resp.StatusCode).Should(Equal(502))
+			Expect(resp.Header.Get("X-Target-Name")).Should(Equal(""))
+		})
 	})
 })
