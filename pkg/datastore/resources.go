6527a7deca4973d44f5ddcb2214fbe0f9d2f5600
Rob Brockbank <rob@tigera.io>
2019-04-13 12:49:57 -0700

    Use AAPIS interface rather than Calico interface to remove the need for Calico access information

--- pkg/datastore/resources.go
+++ pkg/datastore/resources.go
@@ -2,7 +2,6 @@
 package datastore
 
 import (
-	"context"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
@@ -10,8 +9,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	apiv3 "github.com/projectcalico/libcalico-go/lib/apis/v3"
-	"github.com/projectcalico/libcalico-go/lib/options"
 
+	"github.com/tigera/calico-k8sapiserver/pkg/apis/projectcalico/v3"
 	"github.com/tigera/compliance/pkg/resources"
 )
 
@@ -58,39 +57,53 @@ var (
 		},
 		resources.TypeCalicoTiers: {
 			func(c ClientSet) (resources.ResourceList, error) {
-				return c.Tiers().List(context.Background(), options.ListOptions{})
+				return c.Tiers().List(metav1.ListOptions{})
 			},
 		},
 		resources.TypeCalicoHostEndpoints: {
 			func(c ClientSet) (resources.ResourceList, error) {
-				return c.HostEndpoints().List(context.Background(), options.ListOptions{})
+				return c.HostEndpoints().List(metav1.ListOptions{})
 			},
 		},
 		resources.TypeCalicoGlobalNetworkSets: {
 			func(c ClientSet) (resources.ResourceList, error) {
-				return c.GlobalNetworkSets().List(context.Background(), options.ListOptions{})
+				return c.GlobalNetworkSets().List(metav1.ListOptions{})
 			},
 		},
 		resources.TypeCalicoNetworkPolicies: {
-			listNetworkPolicies,
+			listCalicoNetworkPolicies,
 		},
 		resources.TypeCalicoGlobalNetworkPolicies: {
-			func(c ClientSet) (resources.ResourceList, error) {
-				return c.GlobalNetworkPolicies().List(context.Background(), options.ListOptions{})
-			},
+			listCalicoGlobalNetworkPolicies,
 		},
 	}
 )
 
-func listNetworkPolicies(c ClientSet) (resources.ResourceList, error) {
-	// List network policies.
-	npList, err := c.NetworkPolicies().List(context.Background(), options.ListOptions{})
+func listCalicoNetworkPolicies(c ClientSet) (resources.ResourceList, error) {
+	// List tiers.
+	tiers, err := c.Tiers().List(metav1.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
 
+	// List network policies. When going through the AAPIS we need to list on a tier by tier basis.
+	var npList *v3.NetworkPolicyList
+	for _, tier := range tiers.Items {
+		nps, err := c.NetworkPolicies("").List(metav1.ListOptions{
+			LabelSelector: apiv3.LabelTier + "=" + tier.Name,
+		})
+		if err != nil {
+			return nil, err
+		}
+		if npList == nil {
+			npList = nps
+		} else {
+			npList.Items = append(npList.Items, nps.Items...)
+		}
+	}
+
 	// Filter out kubernetes network policies.
-	nps := []apiv3.NetworkPolicy{}
+	nps := []v3.NetworkPolicy{}
 	for _, np := range npList.Items {
 		if strings.HasPrefix(np.Name, "knp.") {
 			log.WithField("np", np.Name).Debug("passing on kubernetes network policy")
@@ -101,5 +114,29 @@ func listNetworkPolicies(c ClientSet) (resources.ResourceList, error) {
 
 	// Over the network policy items with the filtered list.
 	npList.Items = nps
+	return npList, nil
+}
+func listCalicoGlobalNetworkPolicies(c ClientSet) (resources.ResourceList, error) {
+	// List tiers.
+	tiers, err := c.Tiers().List(metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	// List global network policies. When going through the AAPIS we need to list on a tier by tier basis.
+	var npList *v3.GlobalNetworkPolicyList
+	for _, tier := range tiers.Items {
+		nps, err := c.GlobalNetworkPolicies().List(metav1.ListOptions{
+			LabelSelector: apiv3.LabelTier + "=" + tier.Name,
+		})
+		if err != nil {
+			return nil, err
+		}
+		if npList == nil {
+			npList = nps
+		} else {
+			npList.Items = append(npList.Items, nps.Items...)
+		}
+	}
 	return npList, nil
 }
