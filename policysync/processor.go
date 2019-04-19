ff5d7d87e5a502df9d9fec90b21fc03c47016ec3
Neil Jerram <neil@tigera.io>
2019-04-19 19:39:49 +0100

    Don't create domain IP sets in policy sync processor

--- policysync/processor.go
+++ policysync/processor.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Tigera, Inc. All rights reserved.
+// Copyright (c) 2018-2019 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -399,6 +399,9 @@ func (p *Processor) handleNamespaceRemove(update *proto.NamespaceRemove) {
 }
 
 func (p *Processor) handleIPSetUpdate(update *proto.IPSetUpdate) {
+	if update.GetType() == proto.IPSetUpdate_DOMAIN {
+		return
+	}
 	id := update.Id
 	logCxt := log.WithField("ID", id)
 	logCxt.Debug("Processing IPSetUpdate")
