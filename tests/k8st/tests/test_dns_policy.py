0cd7a76235d6230df0d5c73a85cc10a5a347c0f8
Neil Jerram <neil@tigera.io>
2019-04-26 10:53:17 +0100

    Add test for DNS policy
    Also/Including:
    
    - Fix some PEP8 issues
    
    - Add -t to docker run so that curl (within the DNS policy test) sees a TTY
    
    - Declutter the test output a bit
    
      We needed these ls and which commands when getting this new suite
      going, but not now.
    
    - Add note about seeing test output immediately

---
+++ tests/k8st/tests/test_dns_policy.py
@@ -0,0 +1,100 @@
+# Copyright (c) 2019 Tigera, Inc. All rights reserved.
+
+import logging
+
+from tests.k8st.test_base import TestBase
+from tests.k8st.utils.utils import retry_until_success, kubectl, calicoctl
+
+_log = logging.getLogger(__name__)
+
+
+class TestDNSPolicy(TestBase):
+
+    def setUp(self):
+        super(TestDNSPolicy, self).setUp()
+
+        # Create bgp test namespace
+        self.ns = "dns-policy-test"
+        self.create_namespace(self.ns)
+        self.test1 = "test-1 -n " + self.ns
+
+    def tearDown(self):
+        super(TestDNSPolicy, self).tearDown()
+        self.delete_and_confirm(self.ns, "ns")
+
+        # Clean up policy.
+        calicoctl("delete gnp default.allow-egress-to-domain || true")
+        calicoctl("delete gnp default.deny-all-egress-except-dns || true")
+        calicoctl("get gnp")
+
+    def deny_all_egress_except_dns(self):
+        # Deny egress from all pods, except for DNS.
+        calicoctl("""apply -f - << EOF
+apiVersion: projectcalico.org/v3
+kind: GlobalNetworkPolicy
+metadata:
+  name: deny-all-egress-except-dns
+spec:
+  selector: all()
+  types:
+  - Egress
+  egress:
+  - action: Allow
+    protocol: UDP
+    destination:
+      ports:
+      - 53
+  - action: Deny
+EOF
+""")
+
+    def allow_egress_to_domains(self, pod_selector, domains):
+        domain_string = """
+      domains:"""
+        for domain in domains:
+            domain_string = domain_string + """
+      - %s""" % domain
+
+        calicoctl("""apply -f - << EOF
+apiVersion: projectcalico.org/v3
+kind: GlobalNetworkPolicy
+metadata:
+  name: allow-egress-to-domain
+spec:
+  order: 1
+  selector: "%s"
+  types:
+  - Egress
+  egress:
+  - action: Allow
+    destination:%s
+EOF
+""" % (pod_selector, domain_string))
+
+    def test_internet_service(self):
+        kubectl("run " + self.test1 + " --generator=run-pod/v1 " +
+                "--image=laurenceman/alpine")
+        kubectl("wait --for=condition=ready pod/%s" % self.test1)
+
+        def should_connect():
+            kubectl("exec " + self.test1 + " -- " +
+                    "curl --connect-timeout 3 -i -L microsoft.com")
+
+        def should_not_connect():
+            try:
+                should_connect()
+            except Exception:
+                return
+            raise Exception("should not have connected")
+
+        # No policy.
+        should_connect()
+
+        # Deny all egress.
+        self.deny_all_egress_except_dns()
+        retry_until_success(should_not_connect, retries=2)
+
+        # DNS policy.
+        self.allow_egress_to_domains("all()",
+                                     ["microsoft.com", "www.microsoft.com"])
+        retry_until_success(should_connect, retries=2)
