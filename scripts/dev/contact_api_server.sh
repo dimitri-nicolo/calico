e2a1f8e3028cdcd327e2d2b90a2e4ce80d523914
asincu <alina@tigera.io>
2019-06-06 14:12:58 -0700

    [SETUP] Update dev bash scripts

--- scripts/dev/contact_api_server.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-APISERVER=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
-
-echo "Kubernetes Api Server is ${APISERVER}"
--- scripts/dev/register.bash
+++ scripts/dev/register.bash
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
 
-curl "http://localhost:${PORT:-3000}/targets" -X PUT -d 'name=c' -d 'target=http://localhost:5555'
+curl "http://localhost:${VOLTRON_PORT:-5555}/targets" -X PUT -d 'name=cluster1' -d "target=https://${IP_CLUSTER1:-kubernetes.default}"
+curl "http://localhost:${VOLTRON_PORT:-5555}/targets" -X PUT -d 'name=cluster2' -d "target=http://${IP_CLUSTER2:-kubernetes.default}"
 
