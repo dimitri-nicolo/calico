ba0803328bdcf85e7d933b449e4c59b0728b917f
Jade Chunnananda <jade.jch@gmail.com>
2019-06-12 13:34:33 -0700

    [Server] Started Server Tests (#19)
    [Server] x-target tests

--- internal/pkg/server/server_test.go
+++ internal/pkg/server/server_test.go
@@ -2,8 +2,11 @@ package server_test
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"sync"
 
@@ -83,6 +86,97 @@ var _ = Describe("Server", func() {
 		Expect(cerr).NotTo(HaveOccurred())
 		wg.Wait()
 		Expect(err).To(HaveOccurred())
+		close(done)
+	})
+})
+
+var _ = Describe("Server Header Test", func() {
+	var (
+		err error
+		wg  sync.WaitGroup
+		srv *server.Server
+		lis net.Listener
+		ts  *httptest.Server
+	)
+
+	It("should start a server", func() {
+		var e error
+		lis, e = net.Listen("tcp", "localhost:0")
+		Expect(e).NotTo(HaveOccurred())
+
+		srv, e = server.New()
+		Expect(err).NotTo(HaveOccurred())
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err = srv.ServeHTTP(lis)
+		}()
+	})
+
+	Context("when server is up", func() {
+		It("Should not proxy anywhere - return 400", func() {
+			resp, err := http.Get("http://" + lis.Addr().String() + "/")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(400))
+		})
+
+		It("should be able to register a new cluster & start test server", func() {
+			ts = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprint(w, "Hello clusterA")
+			}))
+
+			addCluster(lis.Addr().String(), "clusterA", ts.URL)
+		})
+
+		It("Should not proxy anywhere - no header", func() {
+			resp, err := http.Get("http://" + lis.Addr().String() + "/")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(400))
+		})
+
+		It("Should proxy to clusterA", func() {
+			req, err := http.NewRequest("GET", "http://"+lis.Addr().String()+"/", nil)
+			Expect(err).NotTo(HaveOccurred())
+			req.Header.Add("X-Target", "clusterA")
+			resp, err := http.DefaultClient.Do(req)
+			Expect(err).NotTo(HaveOccurred())
+
+			message, err := ioutil.ReadAll(resp.Body)
+
+			Expect(err).NotTo(HaveOccurred())
+			resp.Body.Close()
+
+			Expect(string(message)).To(Equal("Hello clusterA"))
+		})
+
+		It("Should not proxy anywhere - invalid cluster", func() {
+			req, err := http.NewRequest("GET", "http://"+lis.Addr().String()+"/", nil)
+			Expect(err).NotTo(HaveOccurred())
+			req.Header.Add("X-Target", "zzzzzzz")
+			resp, err := http.DefaultClient.Do(req)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(400))
+		})
+
+		It("Should not proxy anywhere - multiple headers", func() {
+			req, err := http.NewRequest("GET", "http://"+lis.Addr().String()+"/", nil)
+			Expect(err).NotTo(HaveOccurred())
+			req.Header.Add("X-Target", "clusterA")
+			req.Header.Add("X-Target", "helloworld")
+			resp, err := http.DefaultClient.Do(req)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(400))
+		})
+	})
+
+	It("should stop the servers", func(done Done) {
+		cerr := srv.Close()
+		Expect(cerr).NotTo(HaveOccurred())
+		wg.Wait()
+		Expect(err).To(HaveOccurred())
+		if ts != nil {
+			ts.Close()
+		}
 		close(done)
 	})
 })
