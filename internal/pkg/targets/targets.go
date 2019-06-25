1e76d412f76fd4feaf8eb34130379cc0b8ab75d8
Alina Militaru <41362174+asincu@users.noreply.github.com>
2019-06-24 17:07:22 -0700

    Adding comments to comply with golint (#66)
    * Adding comments to comply with golint
    
    * Remove unused selector.go

--- internal/pkg/targets/targets.go
+++ internal/pkg/targets/targets.go
@@ -7,20 +7,25 @@ import (
 	"net/url"
 )
 
+// Targets maps a proxy pattern to a destination
 type Targets struct {
 	targets map[string]*url.URL
 }
 
+// New returns new Targets creates from a prepopulated map
 func New(targets map[string]*url.URL) *Targets {
 	return &Targets{targets: targets}
 }
 
+// NewEmpty returns new empty Targets
 func NewEmpty() *Targets {
 	return &Targets{
 		targets: make(map[string]*url.URL),
 	}
 }
 
+// Add will add a new key-value pair composed of proxy target and destination
+// Add will return an error in case the destination is not a valid URL
 func (t *Targets) Add(target string, dest string) error {
 	url, err := url.Parse(dest)
 	if err != nil {
@@ -31,6 +36,7 @@ func (t *Targets) Add(target string, dest string) error {
 	return nil
 }
 
+// List will list the key-value pairs composed of proxy targets and destinations
 func (t *Targets) List() map[string]*url.URL {
 	return t.targets
 }
