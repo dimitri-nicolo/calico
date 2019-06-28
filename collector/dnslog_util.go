180cfc57cf0567df97a3a7cc6f65c111fcf30d8d
Henry Stern <henry@tigera.io>
2019-06-28 13:39:32 +0000

    Add newfrom function

---
+++ collector/dnslog_util.go
@@ -0,0 +1,85 @@
+package collector
+
+import (
+	"errors"
+	"sort"
+	"strings"
+
+	"github.com/google/gopacket/layers"
+)
+
+func NewDNSMetaSpecFromGoPacket(dns *layers.DNS) (*DNSMeta, *DNSSpec, error) {
+	if len(dns.Questions) == 0 {
+		return nil, nil, errors.New("No questions in DNS packet")
+	}
+
+	spec := &DNSSpec{
+		RRSets:       make(DNSRRSets),
+		Servers:      make(map[EndpointMetadata]DNSLabels),
+		ClientLabels: nil,
+		DNSStats: DNSStats{
+			Count: 1,
+		},
+	}
+
+	for _, rr := range append(append(dns.Answers, dns.Additionals...), dns.Authorities...) {
+		name := DNSName{
+			Name:  canonicalizeDNSName(rr.Name),
+			Class: DNSClass(rr.Class),
+			Type:  DNSType(rr.Type),
+		}
+
+		rdata := DNSRData{
+			Raw:     rr.Data,
+			Decoded: getRRDecoded(rr),
+		}
+
+		spec.RRSets[name] = append(spec.RRSets[name], rdata)
+	}
+	for _, rrset := range spec.RRSets {
+		sort.Stable(rrset)
+	}
+
+	// TODO server metadata
+
+	// TODO client labels
+
+	meta := &DNSMeta{
+		// TODO ClientMeta
+		Question: DNSName{
+			Name:  canonicalizeDNSName(dns.Questions[0].Name),
+			Class: DNSClass(dns.Questions[0].Class),
+			Type:  DNSType(dns.Questions[0].Type),
+		},
+		ResponseCode: dns.ResponseCode,
+		RRSetsString: spec.RRSets.String(),
+	}
+	return meta, spec, nil
+}
+
+func getRRDecoded(rr layers.DNSResourceRecord) interface{} {
+	switch rr.Type {
+	case layers.DNSTypeA, layers.DNSTypeAAAA:
+		return rr.IP
+	case layers.DNSTypeNS:
+		return rr.NS
+	case layers.DNSTypeCNAME:
+		return rr.CNAME
+	case layers.DNSTypePTR:
+		return rr.PTR
+	case layers.DNSTypeTXT:
+		return rr.TXTs
+	case layers.DNSTypeSOA:
+		return rr.SOA
+	case layers.DNSTypeSRV:
+		return rr.SRV
+	case layers.DNSTypeMX:
+		return rr.MX
+	default:
+		return rr.Data
+	}
+}
+
+func canonicalizeDNSName(name []byte) string {
+	return strings.ToLower(strings.TrimRight(string(name), "."))
+}
