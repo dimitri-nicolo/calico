4b977af26b7d89a55d3c2485e7462afe66e2f1a2
ozdanborne <dan@projectcalico.org>
2019-07-15 13:30:28 -0700

    remove unused types

--- pkg/pip/types.go
+++ pkg/pip/types.go
@@ -1,16 +1,5 @@
 package pip
 
-const (
-	PreviewActionPass    = "pass"
-	PreviewActionAllow   = "allow"
-	PreviewActionDeny    = "deny"
-	PreviewActionUnknown = "unknown"
-
-	ChangeActionCreate = "create"
-	ChangeActionDelete = "delete"
-	ChangeActionUpdate = "update"
-)
-
 type contextKey int
 
 const PolicyImpactContextKey contextKey = iota
