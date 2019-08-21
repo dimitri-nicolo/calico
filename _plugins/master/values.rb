06b22c5c9af882e159c2d3459a13006cc1c79453
ozdanborne <dan@projectcalico.org>
2019-08-21 10:31:50 -0700

    remove platform flag from second chart

--- _plugins/master/values.rb
+++ _plugins/master/values.rb
@@ -379,9 +379,6 @@ def gen_chart_specific_values_master(versions, imageNames, imageRegistry, chart,
     # imagePullSecrets:
     #   <secret_name>: <.docker/config.json contents>
     imagePullSecrets: {}
-    
-    # TODO: move to helpers
-    platform: ""
     EOF
   else
     versionsYml = <<~EOF
