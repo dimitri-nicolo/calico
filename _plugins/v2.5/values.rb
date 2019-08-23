a719df0a5b232aa2662e503f49f9f98a41f8cc4e
ozdanborne <dan@projectcalico.org>
2019-08-23 11:20:33 -0700

    add autoscalers to images for v2.5

--- _plugins/v2.5/values.rb
+++ _plugins/v2.5/values.rb
@@ -591,6 +591,14 @@ def gen_chart_specific_values_v2_5(versions, imageNames, imageRegistry, chart, f
       image: #{imageNames["flexvol"]}
       tag: #{versions["flexvol"]}
 
+    cpHorizontalAutoscaler:
+      image: #{imageNames["cpHorizontalAutoscaler"]}
+      tag: #{versions["cpHorizontalAutoscaler"]}
+
+    cpVerticalAutoscaler:
+      image: #{imageNames["cpVerticalAutoscaler"]}
+      tag: #{versions["cpVerticalAutoscaler"]}
+
     # Optional configuration for setting custom BGP templates
     bgp:
       birdConfigTemplate:
