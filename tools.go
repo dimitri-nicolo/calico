11a86cb6958cb41d99aadc598073baaa77ea3113
Neil Jerram <neil@tigera.io>
2019-09-13 11:55:15 +0100

    Take tools.go and test/test.go from Rafael/Casey's merge

--- tools.go
+++ tools.go
@@ -2,6 +2,7 @@
 
 /*
 Copyright 2019 The Kubernetes Authors.
+Copyright (c) 2019 Tigera, Inc. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -29,6 +30,7 @@ import (
 	_ "k8s.io/code-generator/cmd/import-boss"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
+	_ "k8s.io/code-generator/cmd/openapi-gen"
 	_ "k8s.io/code-generator/cmd/register-gen"
 	_ "k8s.io/code-generator/cmd/set-gen"
 )
