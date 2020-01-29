30cbf22789de7de2175019dea1d8532c8fdb374e
tuti <tuti@tigera.io>
2020-01-29 11:55:07 -0800

    update initialized variables

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,7 +11,7 @@ GITHUB_API_TOKEN = os.environ.get('GITHUB_API_TOKEN') or os.environ.get('GITHUB_
 # quay
 QUAY_REGISTRY = 'quay.io'
 QUAY_API_URL = os.environ.get('QUAY_API_URL') or 'https://{}/api/v1'.format(QUAY_REGISTRY)
-QUAY_API_TOKEN = os.environ.get('QUAY_API_TOKEN', 'fake-token')
+QUAY_API_TOKEN = os.environ.get('QUAY_API_TOKEN') or 'fake-token'
 
 REGISTRY = os.environ.get('REGISTRY', QUAY_REGISTRY)
 
@@ -19,5 +19,5 @@ REGISTRY = os.environ.get('REGISTRY', QUAY_REGISTRY)
 HELM_CHARTS_BASE_URL = os.environ.get('HELM_CHARTS_BASE_URL', 'https://s3.amazonaws.com/tigera-public/ee/charts')
 HELM_CORE_BASE_NAME = os.environ.get('HELM_CORE_BASE_NAME', 'tigera-secure-ee-core')
 HELM_EE_BASE_NAME = os.environ.get('HELM_CORE_BASE_NAME', 'tigera-secure-ee')
-HELM_CORE_URL = os.environ.get('HELM_CORE_URL') or '{charts_base_url}/' + HELM_CORE_BASE_NAME + '-{release_version}-{helm_release}.tgz'
-HELM_EE_URL = os.environ.get('HELM_EE_URL') or '{charts_base_url}/' + HELM_EE_BASE_NAME + '-{release_version}-{helm_release}.tgz'
+HELM_CORE_URL = os.environ.get('HELM_CORE_URL', '{charts_base_url}/' + HELM_CORE_BASE_NAME + '-{release_version}-{helm_release}.tgz')
+HELM_EE_URL = os.environ.get('HELM_EE_URL', '{charts_base_url}/' + HELM_EE_BASE_NAME + '-{release_version}-{helm_release}.tgz')
