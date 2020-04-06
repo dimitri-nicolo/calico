28833fc26f47c270b9411e3b419f785eaaa36407
Neil Jerram <neil@tigera.io>
2020-04-06 17:24:19 +0100

    Add egress IP UT cases for PolicyResolver

---
+++ calc/policy_resolver_test.go
@@ -0,0 +1,91 @@
+// Copyright (c) 2020 Tigera, Inc. All rights reserved.
+
+package calc
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/projectcalico/libcalico-go/lib/backend/api"
+	"github.com/projectcalico/libcalico-go/lib/backend/model"
+)
+
+var _ = Describe("PolicyResolver", func() {
+	var (
+		uut *PolicyResolver
+		cbs *prCallbacks
+	)
+
+	BeforeEach(func() {
+		uut = NewPolicyResolver()
+		cbs = &prCallbacks{}
+		uut.RegisterCallback(cbs)
+	})
+
+	It("handles egress IP set ID", func() {
+
+		By("OnUpdate for local endpoint")
+		we1Key := model.WorkloadEndpointKey{
+			WorkloadID: "we1",
+		}
+		uut.OnUpdate(api.Update{
+			KVPair: model.KVPair{
+				Key: we1Key,
+				Value: &model.WorkloadEndpoint{
+					Name: "we1",
+				},
+			},
+			UpdateType: api.UpdateTypeKVNew,
+		})
+		uut.OnDatamodelStatus(api.InSync)
+
+		// Expect OnEndpointTierUpdate with no egress ID.
+		cbs.ExpectEndpointTierUpdate(we1Key, "")
+
+		By("OnEgressIPSetIDUpdate for that endpoint")
+		uut.OnEgressIPSetIDUpdate(we1Key, "e:abcdef")
+
+		// Expect OnEndpointTierUpdate with that egress IP set ID.
+		cbs.ExpectEndpointTierUpdate(we1Key, "e:abcdef")
+
+		By("another OnUpdate for same WE")
+		uut.OnUpdate(api.Update{
+			KVPair: model.KVPair{
+				Key: we1Key,
+				Value: &model.WorkloadEndpoint{
+					Name: "we1",
+				},
+			},
+			UpdateType: api.UpdateTypeKVUpdated,
+		})
+		cbs.ExpectEndpointTierUpdate(we1Key, "e:abcdef")
+
+		By("OnEgressIPSetIDUpdate for that endpoint with no egress IP set")
+		uut.OnEgressIPSetIDUpdate(we1Key, "")
+		cbs.ExpectEndpointTierUpdate(we1Key, "")
+	})
+})
+
+type prCallbacks struct {
+	keys           []model.Key
+	egressIPSetIDs []string
+}
+
+func (tc *prCallbacks) OnEndpointTierUpdate(endpointKey model.Key, endpoint interface{}, egressIPSetID string, filteredTiers []tierInfo) {
+	tc.keys = append(tc.keys, endpointKey)
+	tc.egressIPSetIDs = append(tc.egressIPSetIDs, egressIPSetID)
+}
+
+func (tc *prCallbacks) ExpectEndpointTierUpdate(key model.WorkloadEndpointKey, egressIPSetID string) {
+	Expect(len(tc.keys)).To(BeNumerically(">=", 1))
+	Expect(len(tc.egressIPSetIDs)).To(BeNumerically(">=", 1))
+	Expect(tc.keys[0].(model.WorkloadEndpointKey)).To(Equal(key))
+	Expect(tc.egressIPSetIDs[0]).To(Equal(egressIPSetID))
+	tc.keys = tc.keys[1:]
+	tc.egressIPSetIDs = tc.egressIPSetIDs[1:]
+}
+
+func (tc *prCallbacks) ExpectNoMoreCallbacks() {
+	Expect(len(tc.keys)).To(BeZero())
+	Expect(len(tc.egressIPSetIDs)).To(BeZero())
+}
