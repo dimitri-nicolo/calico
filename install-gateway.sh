b648888b545af6b9eb7526fa9bd75edca1486642
Neil Jerram <neil@tigera.io>
2020-05-11 12:23:41 +0100

    Honour IPTABLES_BACKEND variable, defaulting to "legacy"

--- install-gateway.sh
+++ install-gateway.sh
@@ -9,6 +9,12 @@ trap 'echo "INT received, simply exiting..."; exit 0' INT
 trap 'echo "TERM received, simply exiting..."; exit 0' TERM
 trap 'echo "HUP received, simply exiting..."; exit 0' HUP
 
+# IPTABLES_BACKEND may be set to "legacy" (default) or "nft".  The
+# "legacy" default should always work, until Linux is eventually
+# modified not to support the legacy mode any more.
+: ${IPTABLES_BACKEND:=legacy}
+iptables=iptables-${IPTABLES_BACKEND}
+
 : ${EGRESS_VXLAN_VNI:=4097}
 
 : ${EGRESS_VXLAN_PORT:=4790}
@@ -23,7 +29,7 @@ MAC=`echo $EGRESS_POD_IP | awk -F. '{printf "a2:2a:%02x:%02x:%02x:%02x", $1, $2,
 echo Egress VXLAN VNI: $EGRESS_VXLAN_VNI  VXLAN PORT: $EGRESS_VXLAN_PORT VXLAN MAC: $MAC Pod IP: $EGRESS_POD_IP
 
 echo Configure iptable rules
-iptables -t nat -A POSTROUTING -j MASQUERADE
+${iptables} -t nat -A POSTROUTING -j MASQUERADE
 
 echo Configure vxlan tunnel device
 ip link add vxlan0 type vxlan id $EGRESS_VXLAN_VNI dstport $EGRESS_VXLAN_PORT dev eth0
