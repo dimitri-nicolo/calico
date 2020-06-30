71b5774e345fb76de0628cbb9c6c3a8340bd0bb1
Manoj Ahuje <manoj@tigera.io>
2020-06-30 08:01:33 -0700

    adding sysctl and curl

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,12 @@
-#core elastic dependancies are zlib, ncurses, bash, glibc, coreutils, libstdc++, libgcc
-#force remove other system packages
-rpm -e tar curl libcurl file-libs libdb-utils procps-ng libssh2 libsmartcols krb5-libs libblkid libuuid libmount util-linux python python-libs glib2 binutils lz4 libxml2 libxml2-python readline elfutils-libs nss-sysinit nss-tools expat vim-minimal elfutils-default-yama-scope ncurses-base openldap libidn gnupg2 gpgme libpng libtasn1 json-c  systemd-libs freetype shadow-utils bind-license systemd sqlite --nodeps
+#force remove system packages
+#Readiness probe needs `curl libcurl libcrypt libnspr`
+#sysctl needs systemd-libs procps-ng libelf libz/bz2 pcre systemd libcap
+rpm -e tar file-libs libdb-utils libssh2 libsmartcols krb5-libs libblkid libuuid libmount util-linux python python-libs glib2 binutils lz4 libxml2 libxml2-python readline elfutils-libs nss-sysinit nss-tools expat vim-minimal elfutils-default-yama-scope ncurses-base openldap libidn gnupg2 gpgme libpng libtasn1 json-c freetype shadow-utils bind-license sqlite --nodeps
 
 #cleanup yum traces for libs
 #packages are installed using yum
-for i in tar curl libcurl file-libs libdb-utils procps-ng libssh2 libsmartcols krb5-libs libblkid libuuid libmount util-linux python python-libs glib2 binutils lz4 libxml2 libxml2-python readline elfutils-libs nss-sysinit nss-tools expat vim-minimal elfutils-default-yama-scope ncurses-base openldap libidn gnupg2 gpgme libpng libtasn1 json-c systemd-libs sqlite freetype shadow-utils bind-license systemd nss libdb dbus elfutils-libelf bzip2-libs lua rpm nspr pcre2
+for i in tar file-libs libdb-utils libssh2 libsmartcols krb5-libs libblkid libuuid libmount util-linux python python-libs glib2 binutils lz4 libxml2 libxml2-python readline elfutils-libs nss-sysinit nss-tools expat vim-minimal elfutils-default-yama-scope ncurses-base ncurses-libs openldap libidn gnupg2 gpgme libpng libtasn1 json-c sqlite freetype shadow-utils bind-license nss libdb dbus elfutils-libelf bzip2-libs lua rpm nspr pcre2
+
 do
 	echo "$i"
 	find /var/lib/yum/ -depth -name "*$i*" -type d -exec rm -r "{}" \;
@@ -16,14 +18,6 @@ done
 find /usr/lib64/ -depth -name "*lua*" -type f -exec rm -r "{}" \;
 #libdb libdbus
 find /usr/lib64/ -depth -name "*libdb*" -exec rm -r "{}" \;
-#libelf
-find /usr/lib64/ -depth -name "*libelf*" -exec rm -r "{}" \;
-#bzip2-libs
-find /usr/lib64/ -depth -name "*libbz2*" -exec rm -r "{}" \;
-#nspr
-find /usr/lib64/ -depth -name "*libnspr4*" -exec rm -r "{}" \;
-#libcrypt
-find /usr/lib64/ -depth -name "*libcrypt*" -exec rm -r "{}" \;
 
 #remove this script from fs
 rm "$0"
