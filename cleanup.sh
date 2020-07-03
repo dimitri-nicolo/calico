279ce0b59b6bd3589bcdbdca48d676a57a89bc3b
Manoj Ahuje <manoj@tigera.io>
2020-07-03 15:59:16 -0700

    removing yum cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -5,19 +5,9 @@ do
 	packagefiles+=($OUTPUT)
 done
 
-
 #force remove system packages
 #elastic readiness probe shell script needs curl and bash.
 rpm -e tar file-libs libdb-utils procps-ng libssh2 libsmartcols krb5-libs libblkid libuuid libmount util-linux python python-libs glib2 binutils lz4 libxml2 libxml2-python readline elfutils-libs nss-sysinit nss-tools expat vim-minimal elfutils-default-yama-scope ncurses-base openldap libidn gnupg2 gpgme libpng libtasn1 json-c systemd-libs freetype shadow-utils bind-license systemd sqlite --nodeps
-
-#cleanup yum traces for libs
-#packages are installed using yum
-for i in tar file-libs libdb-utils libssh2 libsmartcols krb5-libs libblkid libuuid libmount util-linux python python-libs glib2 binutils lz4 libxml2 libxml2-python readline elfutils-libs nss-sysinit nss-tools expat vim-minimal elfutils-default-yama-scope ncurses-base ncurses-libs openldap libidn gnupg2 gpgme libpng libtasn1 json-c systemd-libs sqlite freetype shadow-utils bind-license systemd nss libdb dbus elfutils-libelf bzip2-libs lua rpm nspr pcre2
-
-do
-	#remove directory traces
-	find /var/lib/yum/ -depth -name "*$i*" -type d -exec rm -r "{}" \;
-done
 
 #cleanup binaries which can not be removed by rpm
 echo 'Removing related package paths:'
