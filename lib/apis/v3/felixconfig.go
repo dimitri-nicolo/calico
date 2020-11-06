e97fdc0fc6988a3dd08ec210d889222c14048c25
sridhar <sridhar@tigera.io>
2020-11-06 14:36:24 +0530

    Add BPFKprobeEnabled flag

--- lib/apis/v3/felixconfig.go
+++ lib/apis/v3/felixconfig.go
@@ -319,6 +319,8 @@ type FelixConfigurationSpec struct {
 
 	// BPFEnabled, if enabled Felix will use the BPF dataplane. [Default: false]
 	BPFEnabled *bool `json:"bpfEnabled,omitempty" validate:"omitempty"`
+	// BPFKprobeEnabled, if enabled Felix will load the kprobe BPF programs and attach it to functions. [Default: false]
+	BPFKprobeEnabled *bool `json:"bpfKprobeEnabled,omitempty" validate:"omitempty"`
 	// BPFDisableUnprivileged, if enabled, Felix sets the kernel.unprivileged_bpf_disabled sysctl to disable
 	// unprivileged use of BPF.  This ensures that unprivileged users cannot access Calico's BPF maps and
 	// cannot insert their own BPF programs to interfere with Calico's. [Default: true]
