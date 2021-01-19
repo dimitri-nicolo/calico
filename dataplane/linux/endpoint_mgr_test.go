a649dc3d1088a71686f0cd065ed7e0ae32b5068a
Neil Jerram <neil@tigera.io>
2021-01-19 18:23:40 +0000

    Fix up existing UT

--- dataplane/linux/endpoint_mgr_test.go
+++ dataplane/linux/endpoint_mgr_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2020 Tigera, Inc. All rights reserved.
+// Copyright (c) 2017-2021 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -829,6 +829,7 @@ func endpointManagerTests(ipVersion uint8) func() {
 				mockProcSys.write,
 				mockProcSys.stat,
 				false,
+				nil,
 				newCallbacks(),
 				nlHandle,
 			)
