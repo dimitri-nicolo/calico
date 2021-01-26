8fd017867194ed6dd6dcefaf2bd9649c8af0cdb5
Neil Jerram <neil@tigera.io>
2021-01-26 17:49:04 +0000

    Add UT for DeleteMapEntry and DeleteMapEntryIfExists

--- bpf/ut/attach_test.go
+++ bpf/ut/attach_test.go
@@ -1,6 +1,6 @@
 // +build !windows
 
-// Copyright (c) 2020 Tigera, Inc. All rights reserved.
+// Copyright (c) 2020-2021 Tigera, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
