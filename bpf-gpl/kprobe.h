7a3a8f6f653745ac5ffac0c6c2dbc3d3bc8c95ad
Tomas Hruby <tomas@tigera.io>
2021-02-04 11:23:49 -0800

    bpf/kprobes: avoid memcpy, read IPs into the map key
    No need for the compiler to zero both the temp buffer and the key and
    then copy from the temp into the key.

--- bpf-gpl/kprobe.h
+++ bpf-gpl/kprobe.h
@@ -23,11 +23,11 @@
 
 #define SEND_DATA_INTERVAL 10000000000
 struct __attribute__((__packed__)) calico_kprobe_stats_key {
-	__u32 pid;
 	__u8  saddr[16];
-	__u16 sport;
 	__u8  daddr[16];
+	__u16 sport;
 	__u16 dport;
+	__u32 pid;
 	__u16 proto;
 	__u16  dir;
 };
@@ -43,7 +43,7 @@ CALI_MAP(cali_kpstats, 2,
 		511000, 0, MAP_PIN_GLOBAL)
 
 static int CALI_BPF_INLINE ip_addr_is_zero(__u8 *addr) {
-	__u64 *a64 = (__u64*)addr; 
+	__u64 *a64 = (__u64*)addr;
 
 	return (a64[0] == 0 && a64[1] == 0);
 }
@@ -55,7 +55,6 @@ static int CALI_BPF_INLINE kprobe_collect_stats(struct pt_regs *ctx,
 						__u16 tx)
 {
 	__u16 family = 0;
-	__u8 saddr[16] = {0}, daddr[16] = {0};
 	__u64 ts = 0, diff = 0;
 	int ret = 0;
 	struct calico_kprobe_stats_value value = {};
@@ -68,11 +67,11 @@ static int CALI_BPF_INLINE kprobe_collect_stats(struct pt_regs *ctx,
 
 	bpf_probe_read(&family, 2, &sk_cmn->skc_family);
 	if (family == 2 /* AF_INET */) {
-		bpf_probe_read(saddr, 4, &sk_cmn->skc_rcv_saddr);
-		bpf_probe_read(daddr, 4, &sk_cmn->skc_daddr);
+		bpf_probe_read(key.saddr, 4, &sk_cmn->skc_rcv_saddr);
+		bpf_probe_read(key.daddr, 4, &sk_cmn->skc_daddr);
 	} else if (family == 10 /* AF_INET6 */) {
-		bpf_probe_read(saddr, 16, sk_cmn->skc_v6_rcv_saddr.in6_u.u6_addr8);
-		bpf_probe_read(daddr, 16, sk_cmn->skc_v6_daddr.in6_u.u6_addr8);
+		bpf_probe_read(key.saddr, 16, sk_cmn->skc_v6_rcv_saddr.in6_u.u6_addr8);
+		bpf_probe_read(key.daddr, 16, sk_cmn->skc_v6_daddr.in6_u.u6_addr8);
 	} else {
 		CALI_DEBUG("unknown IP family.Ignoring\n");
 		return 0;
@@ -86,7 +85,7 @@ static int CALI_BPF_INLINE kprobe_collect_stats(struct pt_regs *ctx,
 	 * state. Further data cannot be correlated in felix.
 	 */
 
-	if (!key.sport || !key.dport || ip_addr_is_zero(saddr) || ip_addr_is_zero(daddr)) {
+	if (!key.sport || !key.dport || ip_addr_is_zero(key.saddr) || ip_addr_is_zero(key.daddr)) {
 		return 0;
 	}
 
@@ -95,11 +94,6 @@ static int CALI_BPF_INLINE kprobe_collect_stats(struct pt_regs *ctx,
 	if (family == 2) {
 		// v4Inv6Prefix {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xff, 0xff}
 		key.saddr[10] = key.saddr[11] = key.daddr[10] = key.daddr[11] = 0xff;
-		__builtin_memcpy(&key.saddr[12], saddr, 4);
-		__builtin_memcpy(&key.daddr[12], daddr, 4);
-	} else {
-		__builtin_memcpy(key.saddr, saddr, 16);
-		__builtin_memcpy(key.daddr, daddr, 16);
 	}
 
 	key.proto = proto;
