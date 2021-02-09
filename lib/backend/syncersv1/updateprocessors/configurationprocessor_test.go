3c469932db28164a1945254973282d94b6850cd9
sridhar <sridhar@tigera.io>
2021-02-08 17:30:20 -0800

    Changes to add new felix config FlowLogsCollectTCPStats

--- lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
+++ lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
@@ -44,7 +44,7 @@ const (
 )
 
 const (
-	numBaseFelixConfigs = 189
+	numBaseFelixConfigs = 190
 )
 
 var _ = Describe("Test the generic configuration update processor and the concrete implementations", func() {
