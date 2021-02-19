af1cdc08669656096c49025a2650418c48d3cf30
Song Jiang <song@tigera.io>
2021-02-19 20:18:46 +0000

    Merge pull request #191 from tigera/song-calilent-v3.5
    Fix host route for VXLAN and 2004 network id comparision
    (cherry picked from commit b06698780deaa0c29124c9c7020fe71c5e4d4419)

--- internal/pkg/utils/winpol/windows_policy_test.go
+++ internal/pkg/utils/winpol/windows_policy_test.go
@@ -1,4 +1,15 @@
-// Copyright (c) 2018-2020 Tigera, Inc. All rights reserved.
+// Copyright (c) 2018-2021 Tigera, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 
 package winpol
 
@@ -40,7 +51,6 @@ func TestCalculateEndpointPolicies(t *testing.T) {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(pols).To(Equal([]json.RawMessage{
 		json.RawMessage(`{"Type": "SomethingElse"}`),
-		json.RawMessage(`{"DestinationPrefix":"10.11.128.13/32","NeedEncap":true,"Type":"ROUTE"}`),
 	}), "OutBoundNAT should have been filtered out")
 
 	t.Log("With NAT enabled, OutBoundNAT should be augmented")
@@ -49,7 +59,6 @@ func TestCalculateEndpointPolicies(t *testing.T) {
 	Expect(pols).To(Equal([]json.RawMessage{
 		json.RawMessage(`{"ExceptionList":["10.96.0.0/12","10.0.1.0/24","10.0.2.0/24","10.11.128.0/19"],"Type":"OutBoundNAT"}`),
 		json.RawMessage(`{"Type": "SomethingElse"}`),
-		json.RawMessage(`{"DestinationPrefix":"10.11.128.13/32","NeedEncap":true,"Type":"ROUTE"}`),
 	}))
 
 	t.Log("With NAT enabled, and no OutBoundNAT stanza, OutBoundNAT should be added")
@@ -61,7 +70,6 @@ func TestCalculateEndpointPolicies(t *testing.T) {
 	Expect(pols).To(Equal([]json.RawMessage{
 		json.RawMessage(`{"Type": "SomethingElse"}`),
 		json.RawMessage(`{"ExceptionList":["10.0.1.0/24","10.0.2.0/24","10.11.128.0/19"],"Type":"OutBoundNAT"}`),
-		json.RawMessage(`{"DestinationPrefix":"10.11.128.13/32","NeedEncap":true,"Type":"ROUTE"}`),
 	}))
 
 	t.Log("With NAT disabled, and no OutBoundNAT stanza, OutBoundNAT should not be added")
@@ -69,7 +77,6 @@ func TestCalculateEndpointPolicies(t *testing.T) {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(pols).To(Equal([]json.RawMessage{
 		json.RawMessage(`{"Type": "SomethingElse"}`),
-		json.RawMessage(`{"DestinationPrefix":"10.11.128.13/32","NeedEncap":true,"Type":"ROUTE"}`),
 	}))
 }
 
