5e407986959e6d53d7e60bd6004b2af74abe890e
matt <mleung975@gmail.com>
2021-03-30 17:26:47 -0700

    Revert changes that allow revisions to be used on List() for CRDs (not ready for 3.6)

--- lib/backend/k8s/resources/customresource.go
+++ lib/backend/k8s/resources/customresource.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021 Tigera, Inc. All rights reserved.
+// Copyright (c) 2017-2019 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -288,8 +288,7 @@ func (c *customK8sResourceClient) List(ctx context.Context, list model.ListInter
 	// Build the request.
 	req := c.restClient.Get().
 		NamespaceIfScoped(namespace, c.namespaced).
-		Resource(c.resource).
-		Param("resourceVersion", revision)
+		Resource(c.resource)
 
 	// If the prefix is specified, look for the resources with the label
 	// of prefix.
