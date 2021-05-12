5840998a102ce7ff3e0f86dbfbd4c0f009b51749
sridhar <sridhar@tigera.io>
2021-05-11 20:06:24 -0700

    Fix UT failures

--- lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
+++ lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
@@ -44,7 +44,7 @@ const (
 )
 
 const (
-	numBaseFelixConfigs = 194
+	numBaseFelixConfigs = 178
 )
 
 var _ = Describe("Test the generic configuration update processor and the concrete implementations", func() {
