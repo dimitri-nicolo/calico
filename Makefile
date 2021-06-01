d1b729c28314bd2b85ca5860c6f26208f4cda0a4
Brian McMahon <brianmcmahon135@gmail.com>
2021-06-01 11:25:44 -0700

    remove extra docker build line (don't pull image), packages that aren't available in cleanup

--- Makefile
+++ Makefile
@@ -13,12 +13,6 @@ RELEASE_REGISTRIES    ?=quay.io
 RELEASE_BRANCH_PREFIX ?=release-calient
 DEV_TAG_SUFFIX        ?=calient-0.dev
 
-BUILD_IMAGE_NAME?=tigera/kibana
-BUILD_IMAGE_TAG?=latest
-BUILD_IMAGE?=$(BUILD_IMAGE_NAME):$(BUILD_IMAGE_TAG)
-
-PUSH_IMAGE_NAME?=gcr.io/unique-caldron-775/cnx/$(BUILD_IMAGE_NAME)
-
 ###############################################################################
 # Download and include Makefile.common
 #   Additions to EXTRA_DOCKER_ARGS need to happen before the include since
@@ -60,24 +54,17 @@ kibana-image: kibana-bootstrap
 		nvm install && nvm use && \
 		yarn build --docker --skip-docker-ubi --no-oss --release'
 
-BUILD_IMAGE_NAME?=tigera/kibana
-BUILD_IMAGE_TAG?=latest
-BUILD_IMAGE?=$(BUILD_IMAGE_NAME):$(BUILD_IMAGE_TAG)
-
-PUSH_IMAGE_NAME?=gcr.io/unique-caldron-775/cnx/$(BUILD_IMAGE_NAME)
-
 KIBANA_VERSION=$(shell jq -r '.version' kibana/package.json)
 
 image: $(KIBANA_IMAGE)
 $(KIBANA_IMAGE):
 	cd docker && KIBANA_VERSION=$(KIBANA_VERSION) bash Dockerfile-template.sh
-	docker build --build-arg GTM_INTEGRATION=$(GTM_INTEGRATION) docker/. -t $(BUILD_IMAGE)
-	docker build --pull -t $(KIBANA_IMAGE):latest-$(ARCH) --build-arg GTM_INTEGRATION=$(GTM_INTEGRATION) --file ./docker/Dockerfile docker
+	docker build --build-arg GTM_INTEGRATION=$(GTM_INTEGRATION) -t $(KIBANA_IMAGE):latest-$(ARCH) --file ./docker/Dockerfile docker/.
 ifeq ($(ARCH),amd64)
 	docker tag $(KIBANA_IMAGE):latest-$(ARCH) $(KIBANA_IMAGE):latest
 endif
 
 compressed-image: image
-	$(MAKE) docker-compress IMAGE_NAME=$(BUILD_IMAGE)
+	$(MAKE) docker-compress IMAGE_NAME=$(KIBANA_IMAGE)
 
 cd: compressed-image cd-common
--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -4,9 +4,9 @@ set +e
 
 # These packages are split up into chunks of dependent packages (more or less).
 PACKAGES+=" python3-libs platform-python python3-libcomps platform-python-setuptools python3-rpm python3-dnf"
-PACKAGES+=" python3-hawkey python3-libdnf python3-gpg python3-unbound platform-python-pip crypto-policies-scripts"
+PACKAGES+=" python3-hawkey python3-libdnf python3-gpg crypto-policies-scripts"
 PACKAGES+=" python3-setuptools-wheel python3-pip-wheel dnf nss-3.53.1-17.el8_3.i686  nss-3.53.1-17.el8_3.x86_64"
-PACKAGES+=" nss-sysinit yum libdnf unbound-libs"
+PACKAGES+=" nss-sysinit yum libdnf"
 
 # Remove systemd and dependencies.
 PACKAGES+=" systemd systemd-udev systemd-pam dracut-squash dracut-network dracut dbus kexec-tools dhcp-client device-mapper"
@@ -16,14 +16,14 @@ PACKAGES+=" iputils trousers trousers-lib kpartx"
 PACKAGES+=" json-c freetype fontconfig libpng kmod bind-export-libs rpm-build-libs kmod-libs openldap ima-evm-utils xz"
 PACKAGES+=" libidn2 gnupg2 gnutls gnupg2 gpgme glib2 librepo elfutils-libs libsolv libmodulemd shadow-utils libsemanage"
 PACKAGES+=" libsolv gettext-libs gettext libcroco cyrus-sasl-lib util-linux libpwquality pam libnsl2 libtirpc iproute"
-PACKAGES+=" kbd gnupg2-smime libsecret pinentry elfutils-debuginfod-client libmodulemd1"
+PACKAGES+=" kbd"
 
 PACKAGES+=" sqlite-libs-3.26.0-11.el8.i686 sqlite-libs-3.26.0-11.el8.x86_64 nss-softokn-3.53.1-17.el8_3.x86_64"
 PACKAGES+=" nss-softokn-3.53.1-17.el8_3.i686 elfutils-default-yama-scope libyaml dbus-common dbus-tools dbus-libs"
-PACKAGES+=" dbus-daemon tar libseccomp rpm-plugin-systemd-inhibit"
+PACKAGES+=" dbus-daemon tar libseccomp"
 
 # Remove rpm and packages that rpm depends on.
-PACKAGES+=" squashfs-tools libtasn1 lz4-libs bzip2-libs ca-certificates krb5-libs libevent openssl-libs openssl"
+PACKAGES+=" squashfs-tools libtasn1 lz4-libs bzip2-libs ca-certificates krb5-libs openssl-libs openssl"
 PACKAGES+=" openssl-pkcs11 libarchive libdb libdb-utils expat curl p11-kit-trust libcurl-minimal libzstd lua-libs"
 PACKAGES+=" libdb file-libs libdb-utils procps-ng libsmartcols libblkid libuuid libmount"
 PACKAGES+=" binutils readline systemd-libs libcomps libmetalink file libfdisk gdbm gawk dhcp-libs openssl libdb"
