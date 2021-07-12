c0d916aa1f8322523c65a6d016449325d7504370
Jiawei Huang <jiawei@tigera.io>
2021-07-12 09:20:59 -0700

    Check the lowest available buffer ratio in readiness probe
    Find the lowest `buffer_available_buffer_space_ratios` from the
    `tigera_secure_ee_flows` outputs instead of calculating the sum of
    `buffer_total_queued_size`. When fluentd isn't able to send buffered
    data to the destination, the queued buffer size is expected to grow.
    Depending on the buffer `@type` configured, it can be in-memory or a
    file.
    
    Checking the available buffer ratio will make the readiness probe less
    likely to fail comparing to the previous implementation. The new logic
    checks the following:
    1. fluentd daemon should be up.
    2. fluentd daemon should response to `monitor_agent` requests.
    3. We have `retry_count` metric in `tigera_secure_ee_flows` output.
    4. Available buffer ratio is above the `LOWER_BOUND` threshold.
    
    The readiness script will monitor the following metrics:
    * tigera_secure_ee_flows
    * tigera_secure_ee_dns
    * tigera_secure_ee_l7
    * tigera_secure_ee_audit_ee
    * tigera_secure_ee_audit_kube
    * tigera_secure_ee_bgp

--- readiness.sh
+++ readiness.sh
@@ -1,38 +1,33 @@
 #!/bin/sh
 set -e -o pipefail
 
-# Location of readiness file which contains elasticsearch metrics
-readiness_destfile="/tmp/readiness_metrics"
+# Make sure fluentd daemon is up.
+if ! pidof fluentd 1>/dev/null; then
+    exit 1
+fi
+
+# Query fluentd monitor_agent metrics
+# curl will return non-zero error code on failure.
 output=$(curl -s "http://localhost:24220/api/plugins.json")
+# Get the current metrics of elasticsearch for flows, dns, l7, audit_ee, audit_kube, bgp.
+# the tigera_secure_ee output contains multiple values.
+tigera_secure_ee=$(echo "$output" | jq -r '.plugins[] | select(.config.index_name) | select(.config.index_name | startswith("tigera_secure_ee"))')
 
-# Get the current metrics of elasticsearch
-retry_count_current=$(echo $output | jq --raw-output '.plugins[] | select(.config.index_name) | select(.config.index_name | test("tigera_secure_ee_flows")) | .retry_count' | awk '{SUM += $0 } END { print SUM }');
-buffer_length_current=$(echo $output | jq --raw-output '.plugins[] | select(.config.index_name) | select(.config.index_name | test("tigera_secure_ee_flows")) | .buffer_total_queued_size' | awk '{SUM += $0 } END { print SUM }');
-
-# Special case: If flow logs has been turned off, then skip the check and return ready 
-if [ -z ${retry_count_current} ]; then
+# Special case: If flow logs has been turned off, then skip the check and return ready.
+retry_count=$(echo "$tigera_secure_ee" | jq -r '.retry_count' | awk '{ SUM += $0 } END { print SUM }')
+if [ -z "$retry_count" ]; then
     echo "Flow logs to ES is disabled, skip the retry/buffer check"
     exit 0
 fi
 
-# Check if earlier metrics present
-if [ ! -f "$readiness_destfile" ]
-then
-    echo "$retry_count_current $buffer_length_current" > "$readiness_destfile"
-    exit 0
+# Verify if we have enough available buffer space left
+# by default, the total buffer size is 512MB in memory or 64GB on disk
+# see: https://docs.fluentd.org/configuration/buffer-section#buffering-parameters
+buffer_avail_ratio=$(echo "$tigera_secure_ee" | jq -r '.buffer_available_buffer_space_ratios' | sort -n | head -n 1)
+LOWER_BOUND_PERCENTAGE=5
+if [ "$buffer_avail_ratio" -lt "$LOWER_BOUND_PERCENTAGE" ]; then
+    # remaining buffer is low so we shouldn't accept new requests
+    exit 1
 fi
 
-# Get the earlier metrics from readiness file
-data=`cat $readiness_destfile`
-retry_count_prev=$(echo $data | cut -d' ' -f1)
-buffer_length_prev=$(echo $data | cut -d' ' -f2)
-
-# Write new metrics in readiness file for further reference
-echo "$retry_count_current $buffer_length_current" > "$readiness_destfile"
-
-# Compare the new metrics with the earlier metrics of elasticsearch
-if [[ $retry_count_current -gt $retry_count_prev || $buffer_length_current -gt $buffer_length_prev ]];
-then
-    exit 1;
-fi
-exit 0;
+exit 0
