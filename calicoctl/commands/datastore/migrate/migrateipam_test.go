bc65ce1045c40a6651b8bf88b1eb03b5e70c338d
Neil Jerram <neil@tigera.io>
2021-07-13 12:54:39 +0100

    Pin update + add DeepPacketInspections() to UT

--- calicoctl/commands/datastore/migrate/migrateipam_test.go
+++ calicoctl/commands/datastore/migrate/migrateipam_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Tigera, Inc. All rights reserved.
+// Copyright (c) 2020-2021 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -224,6 +224,10 @@ func (c *MockIPAMClient) GlobalReportTypes() client.GlobalReportTypeInterface {
 }
 
 func (c *MockIPAMClient) GlobalReports() client.GlobalReportInterface {
+	return nil
+}
+
+func (c *MockIPAMClient) DeepPacketInspections() client.DeepPacketInspectionInterface {
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -38,6 +38,6 @@ require (
 )
 
 replace (
-	github.com/projectcalico/libcalico-go => github.com/tigera/libcalico-go-private v1.7.2-0.20210701012819-390db5615ef0
+	github.com/projectcalico/libcalico-go => github.com/tigera/libcalico-go-private v1.7.2-0.20210713040402-b88dfcdb2b2b
 	github.com/sirupsen/logrus => github.com/projectcalico/logrus v1.0.4-calico
 )
