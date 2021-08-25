2f0d8bd5c74920be4feaf9d75f7c5b25cca4abfd
Rene Dekker <rene@tigera.io>
2021-08-24 18:11:02 -0700

    tmp, revert

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/gorilla/mux v1.7.3
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/onsi/ginkgo v1.16.4
-	github.com/onsi/gomega v1.10.1
+	github.com/onsi/gomega v1.16.0
 	github.com/pkg/errors v0.9.1
 	github.com/projectcalico/libcalico-go v1.7.3
 	github.com/prometheus/client_golang v1.10.0 // indirect
--- pkg/middlewares/logging.go
+++ pkg/middlewares/logging.go
@@ -2,6 +2,8 @@
 package middlewares
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 
 	log "github.com/sirupsen/logrus"
@@ -10,6 +12,17 @@ import (
 func logRequestHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Infof("ES Gateway received request for URI %s", r.RequestURI)
+		buf, bodyErr := ioutil.ReadAll(r.Body)
+		if bodyErr != nil {
+			log.Print("bodyErr ", bodyErr.Error())
+			http.Error(w, bodyErr.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		rdr1 := ioutil.NopCloser(bytes.NewBuffer(buf))
+		rdr2 := ioutil.NopCloser(bytes.NewBuffer(buf))
+		log.Printf("BODY: %q", rdr1)
+		r.Body = rdr2
 		next.ServeHTTP(w, r)
 	})
 }
