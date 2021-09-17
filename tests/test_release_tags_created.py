92427d8b02bfc974322ed95bb48e38f502e0ba7b
Sujeet Kumar <sujeet@tigera.io>
2021-09-16 21:23:34 -0400

    added make tests for libcalicogo and calico-private (#3611) (#3619)
    * added framework to include/remove specific repos from specific tests.
    * added make tests for api, libcalicogo and calico-private

--- tests/test_release_tags_created.py
+++ tests/test_release_tags_created.py
@@ -53,6 +53,9 @@ MAPPED_COMPONENTS = {
     'packetcapture-api': 'packetcapture-api',
     'tigera-prometheus-service': 'prometheus-service',
     'es-gateway': 'es-gateway',
+    'calico-private': 'calico-private',
+    'libcalico-go-private': 'libcalico-go-private',
+    'tigera-api': 'api',
 }
 
 SKIP_COMPONENTS = [
@@ -72,6 +75,20 @@ SKIP_COMPONENTS = [
     'alertmanager',
 ]
 
+WITHOUT_IMAGES = [
+    # Release items that do not have an Image
+    'calico-private',
+    'libcalico-go-private',
+    'tigera-api',
+]
+
+WITHOUT_TAGS = [
+    # Release items for which we do not publish tags yet
+    # These items are ignored for tag testing but included for branch testing
+    'libcalico-go-private',
+    'tigera-api',
+]
+
 with open('%s/../_data/versions.yml' % PATH) as f:
     versions = yaml.safe_load(f)
     release = versions[0]
@@ -80,8 +97,7 @@ with open('%s/../_data/versions.yml' % PATH) as f:
 
 def test_all_tigera_images_are_mapped():
 
-    mapped_images = MAPPED_COMPONENTS
-
+    mapped_images = {k: v for k, v in MAPPED_COMPONENTS.items() if k not in WITHOUT_IMAGES }
     version_components = {k: v for k, v in release.get('components').items() if v.has_key('image') and v.get('image').startswith('tigera/') and k not in SKIP_COMPONENTS}
 
     assert len(mapped_images.keys()) == len(version_components.keys())
@@ -106,26 +122,10 @@ def test_component_repo_has_release_branch(name, repo_name):
 @parameterized(MAPPED_COMPONENTS.items())
 def test_component_repo_has_release_tag(name, repo_name):
     assert GITHUB_API_TOKEN != 'fake-token', '[ERROR] need a real GITHUB_API_TOKEN env value'
-
-    print '[INFO] checking {0} repo({1}) has {2} release tag'.format(name, repo_name, RELEASE_STREAM)
-
-    headers = {'Accept': 'application/vnd.github.v3.raw', 'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}
-    req_url = '{base_url}/repos/tigera/{repo}/git/refs/{ref}'.format(
-        base_url=GITHUB_API_URL, repo=repo_name, ref='tags/{}'.format(RELEASE_VERSION))
-    res = requests.head(req_url, headers=headers)
-    assert res.status_code == 200
-
-def test_docs_repo_has_release_branch():
-    assert GITHUB_API_TOKEN != 'fake-token', '[ERROR] need a real GITHUB_API_TOKEN env value'
-
-    release_prefix = EE_RELEASE_BRANCH_PREFIX
-    if RELEASE_STREAM < 'v3.0':
-        release_prefix = 'release'
-
-    print '[INFO] checking calico-private repo has {0}-{1} branch'.format(release_prefix, RELEASE_STREAM)
-
-    headers = {'Accept': 'application/vnd.github.v3.raw', 'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}
-    req_url = '{base_url}/repos/tigera/{repo}/branches/{branch}'.format(
-        base_url=GITHUB_API_URL, repo='calico-private', branch='{0}-{1}'.format(release_prefix, RELEASE_STREAM))
-    res = requests.head(req_url, headers=headers)
-    assert res.status_code == 200
+    if name not in WITHOUT_TAGS:
+        print '[INFO] checking {0} repo({1}) has {2} release tag'.format(name, repo_name, RELEASE_STREAM)
+        headers = {'Accept': 'application/vnd.github.v3.raw', 'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}
+        req_url = '{base_url}/repos/tigera/{repo}/git/ref/{ref}'.format(
+            base_url=GITHUB_API_URL, repo=repo_name, ref='tags/{}'.format(RELEASE_VERSION))
+        res = requests.head(req_url, headers=headers)
+        assert res.status_code == 200
