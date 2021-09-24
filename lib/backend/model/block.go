195e3e1a21f8b79b2ffb2987b83c6da8eaf4aa42
Shaun Crampton <shaun@tigera.io>
2021-09-24 14:18:22 +0100

    Add constant ipam.AttributeTypeAWSSecondary for Felix to use.

--- lib/backend/model/block.go
+++ lib/backend/model/block.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2020 Tigera, Inc. All rights reserved.
+// Copyright (c) 2016-2021 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -29,14 +29,16 @@ import (
 
 const (
 	// Common attributes which may be set on allocations by clients.
-	IPAMBlockAttributePod           = "pod"
-	IPAMBlockAttributeNamespace     = "namespace"
-	IPAMBlockAttributeNode          = "node"
-	IPAMBlockAttributeType          = "type"
-	IPAMBlockAttributeTypeIPIP      = "ipipTunnelAddress"
-	IPAMBlockAttributeTypeVXLAN     = "vxlanTunnelAddress"
-	IPAMBlockAttributeTypeWireguard = "wireguardTunnelAddress"
-	IPAMBlockAttributeTimestamp     = "timestamp"
+	IPAMBlockAttributePod       = "pod"
+	IPAMBlockAttributeNamespace = "namespace"
+	IPAMBlockAttributeNode      = "node"
+
+	IPAMBlockAttributeType             = "type"
+	IPAMBlockAttributeTypeIPIP         = "ipipTunnelAddress"
+	IPAMBlockAttributeTypeVXLAN        = "vxlanTunnelAddress"
+	IPAMBlockAttributeTypeWireguard    = "wireguardTunnelAddress"
+	IPAMBlockAttributeTypeAWSSecondary = "awsSecondaryIface"
+	IPAMBlockAttributeTimestamp        = "timestamp"
 )
 
 var (
