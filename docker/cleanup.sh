fd20445b643c064a8e005b03ceb85a331addd95c
Brian McMahon <brianmcmahon135@gmail.com>
2021-09-28 09:56:57 -0700

    Fix package clean up list

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -3,31 +3,22 @@
 set +e
 
 # These packages are split up into chunks of dependent packages (more or less).
-PACKAGES+=" python3-libs platform-python python3-libcomps platform-python-setuptools python3-rpm python3-dnf"
-PACKAGES+=" python3-hawkey python3-libdnf python3-gpg python3-unbound platform-python-pip crypto-policies-scripts"
-PACKAGES+=" python3-setuptools-wheel python3-pip-wheel dnf nss-3.53.1-17.el8_3.i686  nss-3.53.1-17.el8_3.x86_64"
-PACKAGES+=" nss-sysinit yum libdnf unbound-libs"
-
-# Remove systemd and dependencies.
-PACKAGES+=" systemd systemd-udev systemd-pam dracut-squash dracut-network dracut dbus kexec-tools dhcp-client device-mapper"
-PACKAGES+=" device-mapper-libs os-prober grub2-tools grub2-tools-minimal grubby libkcapi-hmaccalc libkcapi cryptsetup-libs"
-PACKAGES+=" iputils trousers trousers-lib kpartx"
-
-PACKAGES+=" json-c freetype fontconfig libpng kmod bind-export-libs rpm-build-libs kmod-libs openldap ima-evm-utils xz"
-PACKAGES+=" libidn2 gnupg2 gnutls gnupg2 gpgme glib2 librepo elfutils-libs libsolv libmodulemd shadow-utils libsemanage"
-PACKAGES+=" libsolv gettext-libs gettext libcroco cyrus-sasl-lib util-linux libpwquality pam libnsl2 libtirpc iproute"
-PACKAGES+=" kbd elfutils-debuginfod-client pciutils shared-mime-info rdma-core libibverbs libpcap iptables-libs tpm2-tss"
-
-PACKAGES+=" sqlite-libs-3.26.0-13.el8.i686 sqlite-libs-3.26.0-13.el8.x86_64 nss-softokn-3.53.1-17.el8_3.x86_64"
-PACKAGES+=" nss-softokn-3.53.1-17.el8_3.i686 elfutils-default-yama-scope libyaml dbus-common dbus-tools dbus-libs"
-PACKAGES+=" dbus-daemon tar libseccomp rpm-plugin-systemd-inhibit"
-
-# Remove rpm and packages that rpm depends on.
-PACKAGES+=" squashfs-tools libtasn1 lz4-libs bzip2-libs ca-certificates krb5-libs libevent openssl-libs openssl"
-PACKAGES+=" openssl-pkcs11 libarchive libdb libdb-utils expat curl p11-kit-trust libcurl-minimal libzstd lua-libs"
-PACKAGES+=" libdb file-libs libdb-utils procps-ng libsmartcols libblkid libuuid libmount"
-PACKAGES+=" binutils readline systemd-libs libcomps libmetalink file libfdisk gdbm gawk dhcp-libs openssl libdb"
-PACKAGES+=" libdb-utils expat curl p11-kit p11-kit-trust libusbx rpm-libs rpm"
+PACKAGES="bind-export-libs binutils bzip2-libs ca-certificates crypto-policies-scripts cryptsetup-libs curl\
+ cyrus-sasl-lib dbus dbus-common dbus-daemon dbus-libs dbus-tools device-mapper device-mapper-libs dhcp-client\
+ dhcp-libs dnf dracut dracut-network dracut-squash elfutils-debuginfod-client elfutils-default-yama-scope\
+ elfutils-libs expat file file-libs fontconfig freetype gawk gdbm gettext gettext-libs glib2 gnupg2 gnutls gpgme\
+ grub2-tools grub2-tools-efi grub2-tools-extra grub2-tools-minimal grubby ima-evm-utils iproute iptables-libs iputils\
+ json-c kbd kexec-tools kmod kmod-libs kpartx krb5-libs libarchive libblkid libcomps libcroco libcurl-minimal libdb\
+ libdb-utils libdnf libevent libfdisk libibverbs libidn2 libkcapi libkcapi-hmaccalc libmetalink libmodulemd libmount\
+ libnsl2 libpcap libpng libpwquality librepo libseccomp libsemanage libsmartcols libsolv libtasn1 libtirpc libusbx\
+ libuuid libyaml libzstd lua-libs lz4-libs nss-3.67.0-6.el8_4.i686 nss-3.67.0-6.el8_4.x86_64\
+ nss-softokn-3.67.0-6.el8_4.i686 nss-softokn-3.67.0-6.el8_4.x86_64 nss-sysinit openldap openssl openssl-libs\
+ openssl-pkcs11 os-prober p11-kit p11-kit-trust pam pciutils platform-python platform-python-pip\
+ platform-python-setuptools procps-ng python3-dnf python3-gpg python3-hawkey python3-libcomps python3-libdnf\
+ python3-libs python3-pip-wheel python3-rpm python3-setuptools-wheel python3-unbound rdma-core readline rpm\
+ rpm-build-libs rpm-libs rpm-plugin-systemd-inhibit shadow-utils shared-mime-info sqlite-libs-3.26.0-13.el8.i686\
+ sqlite-libs-3.26.0-13.el8.x86_64 squashfs-tools systemd systemd-libs systemd-pam systemd-udev tar tpm2-tss trousers\
+ trousers-lib unbound-libs util-linux xz yum"
 
 echo "$PACKAGES"
 
