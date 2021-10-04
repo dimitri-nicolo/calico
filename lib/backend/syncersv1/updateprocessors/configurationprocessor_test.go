8f2c3d009720c2457f8d383fb3e902d71937f1e4
Shaun Crampton <shaun@tigera.io>
2021-10-04 13:34:52 +0100

    Increase expected number of felix config entries.

--- lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
+++ lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
@@ -46,7 +46,7 @@ const (
 )
 
 const (
-	numBaseFelixConfigs = 188
+	numBaseFelixConfigs = 191
 )
 
 var _ = Describe("Test the generic configuration update processor and the concrete implementations", func() {
