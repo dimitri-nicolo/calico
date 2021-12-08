4735ba9bbedc393c6146209052c645646b651a99
Shaun Crampton <shaun@tigera.io>
2021-12-08 10:23:37 +0000

    Add AWS elastic IP conversion test.

--- lib/backend/k8s/conversion/conversion_test.go
+++ lib/backend/k8s/conversion/conversion_test.go
@@ -183,6 +183,7 @@ var _ = Describe("Test Pod conversion", func() {
 					"arbitrary": "annotation",
 					"egress.projectcalico.org/namespaceSelector": "wblack == 'white'",
 					"egress.projectcalico.org/selector":          "wred == 'green'",
+					"cni.projectcalico.org/awsElasticIPs":        "[\"44.55.66.77\",\"88.55.66.77\"]",
 				},
 				Labels: map[string]string{
 					"labelA": "valueA",
@@ -314,6 +315,7 @@ var _ = Describe("Test Pod conversion", func() {
 			NamespaceSelector: "wblack == 'white'",
 			Selector:          "wred == 'green'",
 		}))
+		Expect(wep.Value.(*libapiv3.WorkloadEndpoint).Spec.AWSElasticIPs).To(ConsistOf("44.55.66.77", "88.55.66.77"))
 	})
 
 	It("should parse a Pod with dual stack IPs to a WorkloadEndpoint", func() {
