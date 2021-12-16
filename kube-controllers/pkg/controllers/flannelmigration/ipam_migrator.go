26a7893b517c06d694a54bb0828a811e72cc3995
Casey Davenport <davenport.cas@gmail.com>
2021-12-16 12:46:07 -0800

    Fix kube-controllers build

--- kube-controllers/pkg/controllers/flannelmigration/ipam_migrator.go
+++ kube-controllers/pkg/controllers/flannelmigration/ipam_migrator.go
@@ -24,7 +24,6 @@ import (
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
 
-	api "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
 	log "github.com/sirupsen/logrus"
 	api "github.com/tigera/api/pkg/apis/projectcalico/v3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
