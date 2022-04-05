8ab71117eca1719517e04457651a6872650a53d4
Dan (Turk) Osborne <dan@projectcalico.org>
2022-04-05 12:48:45 -0400

    report data written even if clusterID is blank (#51)

--- pkg/handlers/elastic.go
+++ pkg/handlers/elastic.go
@@ -15,29 +15,28 @@ func ElasticModifyResponseFunc(collector metrics.Collector) func(res *http.Respo
 		req := res.Request
 		if collector != nil && res.StatusCode >= http.StatusOK && res.StatusCode < http.StatusMultipleChoices {
 			ctx := req.Context()
-			clusterID := ctx.Value(middlewares.ClusterIDKey)
+			clusterID, _ := ctx.Value(middlewares.ClusterIDKey).(string)
 
-			if clusterID != nil && clusterID.(string) != "" {
-				if req.ContentLength > 0 {
-					if err := collector.CollectLogBytesWritten(clusterID.(string), float64(req.ContentLength)); err != nil {
-						log.Errorf("Error occurred while collecting CollectLogBytesRead metrics for request to: %v", req.RequestURI)
-					}
+			if req.ContentLength > 0 {
+				if err := collector.CollectLogBytesWritten(clusterID, float64(req.ContentLength)); err != nil {
+					log.Errorf("Error occurred while collecting CollectLogBytesRead metrics for request to: %v", req.RequestURI)
 				}
-				if res.ContentLength > 0 {
-					if err := collector.CollectLogBytesRead(clusterID.(string), float64(res.ContentLength)); err != nil {
-						log.Errorf("Error occurred while collecting CollectLogBytesRead metrics for request to: %v", req.RequestURI)
-					}
-				}
-				log.Tracef(
-					"Collecting metrics after successful response: %v, url: %v, response: %v, req contentlength: %v, res contentlength: %v, cluster: %v",
-					req.Method,
-					req.URL,
-					req.Response,
-					req.ContentLength,
-					res.ContentLength,
-					clusterID,
-				)
 			}
+			if res.ContentLength > 0 {
+				if err := collector.CollectLogBytesRead(clusterID, float64(res.ContentLength)); err != nil {
+					log.Errorf("Error occurred while collecting CollectLogBytesRead metrics for request to: %v", req.RequestURI)
+				}
+			}
+			log.Tracef(
+				"Collecting metrics after successful response: %v, url: %v, response: %v, req contentlength: %v, res contentlength: %v, cluster: %v",
+				req.Method,
+				req.URL,
+				req.Response,
+				req.ContentLength,
+				res.ContentLength,
+				clusterID,
+			)
+
 		}
 		return nil
 	}
