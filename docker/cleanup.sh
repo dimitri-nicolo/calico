470eac93f5adadcd76614fdaa44e88e78296a934
Rene Dekker <rene@tigera.io>
2022-04-11 13:11:27 -0700

    Remove more libs to make the build pass

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -3,27 +3,132 @@
 set +e
 
 # These packages are split up into chunks of dependent packages (more or less).
-PACKAGES="bzip2-libs ca-certificates
-crypto-policies-scripts cryptsetup-libs curl cyrus-sasl-lib dbus dbus-common
-dbus-daemon dbus-glib dbus-libs dbus-tools device-mapper device-mapper-libs dnf
-elfutils-default-yama-scope elfutils-libs expat file-libs fontconfig
-freetype gawk gdbm glib2 gobject-introspection gnupg2 gnutls gpgme
-ima-evm-utils json-c json-glib kmod-libs krb5-libs libarchive libblkid libcomps
-libcurl libdb libdb-utils libdnf libfdisk libidn2 libmodulemd libmount libnsl2
-libpng libpsl libpwquality librepo librhsm libseccomp libsemanage libsmartcols libsolv libssh
-libtasn1 libtirpc libusbx libuser libuuid libyaml libzstd lua-libs lz4-libs nss-sysinit
+PACKAGES="
+bzip2-libs
+ca-certificates
+crypto-policies-scripts
+cryptsetup-libs
+curl
+cyrus-sasl-lib
+dbus
+dbus-common
+dbus-daemon
+dbus-glib
+dbus-libs
+dbus-tools
+device-mapper
+device-mapper-libs
+dnf
+elfutils-default-yama-scope
+elfutils-libs
+expat
+file-libs
+fontconfig
+freetype
+gawk
+gdbm
+glib2
+gnupg2
+gnutls
+gobject-introspection
+gpgme
+ima-evm-utils
+json-c
+json-glib
+kmod-libs
+krb5-libs
+libarchive
+libblkid
+libcomps
+libcurl
+libdb
+libdb-utils
+libdnf
+libevent-2.1.8-5.el8.x86_64
+libfdisk
+libidn2
+libmodulemd
+libmount
+libnsl2
+libpng
+libpsl
+libpwquality
+librepo
+librhsm
+libseccomp
+libsemanage
+libsmartcols
+libsolv
+libssh
+libtasn1
+libtirpc
+libusbx
+libuser
+libuuid
+libyaml
+libzstd
+lua-libs
+lz4-libs
 nss-3.67.0-7.el8_5.x86_64
-openldap openssl-libs p11-kit p11-kit-trust pam passwd
-platform-python platform-python-setuptools python3-chardet python3-cloud-what python3-dateutil
-python3-dbus python3-decorator python3-dmidecode python3-dnf python3-dnf-plugins-core
-python3-ethtool python3-gobject-base python3-gpg python3-hawkey python3-idna python3-iniparse
-python3-inotify python3-libcomps python3-libdnf python3-librepo python3-libs python3-libxml2
-python3-pip-wheel python3-pysocks python3-requests python3-rpm python3-setuptools-wheel
-python3-six python3-subscription-manager-rhsm python3-syspurpose python3-urllib3
-readline rpm rpm-build-libs rpm-libs
-shadow-utils systemd
-systemd-libs systemd-pam tar tpm2-tss
-usermode util-linux virt-what yum"
+nss-sysinit
+openldap
+openssl-1:1.1.1k-6.el8.x86_64
+openssl-libs
+openssl-pkcs11-0.4.10-2.el8.x86_64
+p11-kit
+p11-kit-trust
+pam
+passwd
+platform-python
+platform-python-pip-9.0.3-22.el8.noarch
+platform-python-setuptools
+python3-chardet
+python3-cloud-what
+python3-dateutil
+python3-dbus
+python3-decorator
+python3-dmidecode
+python3-dnf
+python3-dnf-plugins-core
+python3-ethtool
+python3-gobject-base
+python3-gpg
+python3-hawkey
+python3-idna
+python3-iniparse
+python3-inotify
+python3-libcomps
+python3-libdnf
+python3-librepo
+python3-libs
+python3-libxml2
+python3-pip-wheel
+python3-pysocks
+python3-requests
+python3-rpm
+python3-setuptools-wheel
+python3-six
+python3-subscription-manager-rhsm
+python3-syspurpose
+python3-unbound-1.7.3-17.el8.x86_64
+python3-urllib3
+readline
+rpm
+rpm-build-libs
+rpm-libs
+rpm-plugin-systemd-inhibit-4.14.3-22.el8.x86_64
+shadow-utils
+systemd
+systemd-libs
+systemd-pam
+tar
+tpm2-tss
+unbound-libs-1.7.3-17.el8.x86_64
+usermode
+util-linux
+virt-what
+yum
+"
 
 
 echo "$PACKAGES"
