0988c73991db501f2e35244a819614565f979766
Jiawei Huang <jiawei@tigera.io>
2022-05-19 14:07:46 -0700

    Fix FVs

--- es-proxy/test/setup_elasticsearch_index.sh
+++ es-proxy/test/setup_elasticsearch_index.sh
@@ -22,8 +22,8 @@ function create_index()
   local INDEX_NAME=$1
   local NO_DATE=$2
   # Create the index template first if we have one. Do this before we create the corresponding index.
-  if [ -f "/test/es-templates/$INDEX_NAME" ]; then
-    TEMPLATE=$(cat "/test/es-templates/$INDEX_NAME")
+  if [ -f "/go/src/${PACKAGE_NAME}/test/es-templates/$INDEX_NAME" ]; then
+    TEMPLATE=$(cat "/go/src/${PACKAGE_NAME}/test/es-templates/$INDEX_NAME")
     curl --insecure -f --retry ${RETRY_TIMES} -X PUT \
       "${ELASTIC_SCHEME}://${ELASTIC_HOST}:9200/_template/${INDEX_NAME}.cluster" ${EXTRA_CURL_ARGS} \
       -H 'Content-Type: application/json' -d "$TEMPLATE"
@@ -42,8 +42,8 @@ function create_index()
     "$URL" ${EXTRA_CURL_ARGS}
 
   # Create a document if there is one.
-  if [ -f "/test/es-samples/$INDEX_NAME" ]; then
-    SAMPLE=$(cat "/test/es-samples/$INDEX_NAME")
+  if [ -f "/go/src/${PACKAGE_NAME}/test/es-samples/$INDEX_NAME" ]; then
+    SAMPLE=$(cat "/go/src/${PACKAGE_NAME}/test/es-samples/$INDEX_NAME")
     curl --insecure -f --retry ${RETRY_TIMES} -X POST \
       "${ELASTIC_SCHEME}://${ELASTIC_HOST}:9200/${INDEX_NAME}.${CLUSTER_NAME}.${DATE_SUFFIX}/_doc" ${EXTRA_CURL_ARGS} \
       -H 'Content-Type: application/json' -d "$SAMPLE"
--- es-proxy/test/setup_k8s_auth.sh
+++ es-proxy/test/setup_k8s_auth.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-docker exec st-apiserver kubectl apply -f /test/role.yaml
-docker exec st-apiserver kubectl apply -f /test/token-binding.yaml
+docker exec "${APISERVER_NAME}" kubectl apply -f "/go/src/${PACKAGE_NAME}/test/role.yaml"
+docker exec "${APISERVER_NAME}" kubectl apply -f "/go/src/${PACKAGE_NAME}/test/token-binding.yaml"
 
-docker exec st-apiserver kubectl apply -f /test/pip/roles.yaml
-docker exec st-apiserver kubectl apply -f /test/pip/role-bindings.yaml
+docker exec "${APISERVER_NAME}" kubectl apply -f "/go/src/${PACKAGE_NAME}/test/pip/roles.yaml"
+docker exec "${APISERVER_NAME}" kubectl apply -f "/go/src/${PACKAGE_NAME}/test/pip/role-bindings.yaml"
 
-docker exec st-apiserver kubectl create clusterrole selfsubjectreview \
+docker exec "${APISERVER_NAME}" kubectl create clusterrole selfsubjectreview \
 	--verb=create --resource=selfsubjectaccessreviews.authorization.k8s.io
