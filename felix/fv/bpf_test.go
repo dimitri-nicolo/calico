69f0703705ade80858bcaa1178aac0981026d3fa
Tomas Hruby <49207409+tomastigera@users.noreply.github.com>
2022-06-30 12:05:22 -0700

    Pick: Merge pull request #6289 from tomastigera/tomas-map-upgrade-simpler
    felix/bpf: map upgrades do not need caching map

--- felix/fv/bpf_test.go
+++ felix/fv/bpf_test.go
@@ -853,7 +853,7 @@ func describeBPFTests(opts ...bpfTestOpt) bool {
 					sort.Strings(filteredLines)
 					return strings.Join(filteredLines, "\n")
 				}
-				Eventually(dumpRoutes, "5s").Should(Equal(expectedRoutes), dumpRoutes)
+				Eventually(dumpRoutes, "10s").Should(Equal(expectedRoutes), dumpRoutes)
 			})
 
 			It("should only allow traffic from the local host by default", func() {
