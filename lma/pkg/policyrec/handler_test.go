6c974aa681723d5c78b1fdc3e5d9377cbf2f29ce
Dimitri Nicolopoulos <dimitri@tigera.io>
2022-10-11 17:45:52 -0700

    [EV-2574] Add namespace based policy recommendation.

--- lma/pkg/policyrec/handler_test.go
+++ lma/pkg/policyrec/handler_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Tigera, Inc. All rights reserved.
+// Copyright (c) 2019, 2022 Tigera, Inc. All rights reserved.
 package policyrec_test
 
 import (
@@ -13,12 +13,13 @@ import (
 )
 
 var (
-	properRequestBody       = `{"start_time": "now-3h", "end_time": "now-0h", "endpoint_name": "test-app-pod", "namespace": "test-namespace"}`
-	missingEndpointName     = `{"start_time": "now-3h", "end_time": "now-0h", "namespace": "test-namespace"}`
-	missingStartTime        = `{"end_time": "now-0h", "endpoint_name": "test-app-pod", "namespace": "test-namespace"}`
-	missingEndTime          = `{"start_time": "now-3h", "endpoint_name": "test-app-pod", "namespace": "test-namespace"}`
-	missingNamespace        = `{"start_time": "now-3h", "end_time": "now-0h", "endpoint_name": "test-app-pod"}`
-	improperJSONRequestBody = `"start_time": "now-3h", "end_time": "now-0h", "endpoint_name": "test-app-pod", "namespace": "test-namespace"`
+	properRequestBody           = `{"start_time": "now-3h", "end_time": "now-0h", "endpoint_name": "test-app-pod", "namespace": "test-namespace"}`
+	missingEndpointName         = `{"start_time": "now-3h", "end_time": "now-0h", "namespace": "test-namespace"}`
+	missingStartTime            = `{"end_time": "now-0h", "endpoint_name": "test-app-pod", "namespace": "test-namespace"}`
+	missingEndTime              = `{"start_time": "now-3h", "endpoint_name": "test-app-pod", "namespace": "test-namespace"}`
+	missingNamespace            = `{"start_time": "now-3h", "end_time": "now-0h", "endpoint_name": "test-app-pod"}`
+	missingNamespaceAndEndpoint = `{"start_time": "now-3h", "end_time": "now-0h"}`
+	improperJSONRequestBody     = `"start_time": "now-3h", "end_time": "now-0h", "endpoint_name": "test-app-pod", "namespace": "test-namespace"`
 
 	fullParams = policyrec.PolicyRecommendationParams{
 		StartTime:     "now-3h",
@@ -32,6 +33,14 @@ var (
 		StartTime:     "now-3h",
 		EndTime:       "now-0h",
 		EndpointName:  "test-app-pod",
+		DocumentIndex: "tigera_secure_ee_flows*",
+	}
+
+	namespaceParams = policyrec.PolicyRecommendationParams{
+		StartTime:     "now-3h",
+		EndTime:       "now-0h",
+		EndpointName:  "",
+		Namespace:     "test-namespace",
 		DocumentIndex: "tigera_secure_ee_flows*",
 	}
 )
@@ -56,10 +65,11 @@ var _ = Describe("Policy Recommendation Unit Tests for handler functions", func(
 			}
 		},
 		Entry("Proper request body with all values filled", properRequestBody, fullParams, false),
-		Entry("Request body missing endpoint name", missingEndpointName, nil, true),
+		Entry("Request body missing endpoint name", missingEndpointName, namespaceParams, false),
 		Entry("Request body missing start time", missingStartTime, nil, true),
+		Entry("Request body missing namespace and endpoint", missingNamespaceAndEndpoint, nil, true),
 		Entry("Request body missing end time", missingEndTime, nil, true),
-		Entry("Request body missing namespace (global)", missingNamespace, globalParams, true),
+		Entry("Request body missing namespace (global)", missingNamespace, globalParams, false),
 		Entry("Request body is improperly formatted JSON", improperJSONRequestBody, nil, true),
 	)
 })
