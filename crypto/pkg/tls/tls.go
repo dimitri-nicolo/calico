6914af5d3801ddf5fb5f9e283f883f169a8decca
Rene Dekker <rene@tigera.io>
2022-11-08 16:14:09 -0800

    Log whether fipsmode is enabled whenever a TLS config is created (#5313)
    * Log whether fipsmode is enabled whenever a TLS config is created

--- crypto/pkg/tls/tls.go
+++ crypto/pkg/tls/tls.go
@@ -2,12 +2,15 @@ package tls
 
 import (
 	"crypto/tls"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // NewTLSConfig returns a tls.Config with the recommended default settings for Calico Enterprise components.
 // Read more recommendations here in Chapter 3:
 // https://www.gsa.gov/cdnstatic/SSL_TLS_Implementation_%5BCIO_IT_Security_14-69_Rev_6%5D_04-06-2021docx.pdf
 func NewTLSConfig(fipsMode bool) *tls.Config {
+	log.WithField("fipsMode", fipsMode).Info("creating a TLS config")
 	cfg := &tls.Config{
 		MinVersion: tls.VersionTLS12,
 		MaxVersion: tls.VersionTLS13,
