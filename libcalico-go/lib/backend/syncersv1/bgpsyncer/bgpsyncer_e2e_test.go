e29bc2f83be0426ce4940e143b963afdce3d80b2
song jiang <song@tigera.io>
2022-12-05 16:01:45 +0000

    Update bgpsyncer

--- libcalico-go/lib/backend/syncersv1/bgpsyncer/bgpsyncer_e2e_test.go
+++ libcalico-go/lib/backend/syncersv1/bgpsyncer/bgpsyncer_e2e_test.go
@@ -210,6 +210,25 @@ var _ = testutils.E2eDatastoreDescribe("BGP syncer tests", testutils.DatastoreAl
 			syncTester.ExpectCacheSize(expectedCacheSize)
 			syncTester.ExpectPath("/calico/resources/v3/projectcalico.org/bgppeers/peer1")
 
+			By("Creating an ExternalNetwork")
+			index := uint32(28)
+			_, err = c.ExternalNetwork().Create(
+				ctx,
+				&apiv3.ExternalNetwork{
+					ObjectMeta: metav1.ObjectMeta{Name: "net1"},
+					Spec: apiv3.ExternalNetworkSpec{
+						RouteTableIndex: &index,
+					},
+				},
+				options.SetOptions{},
+			)
+			Expect(err).NotTo(HaveOccurred())
+
+			// The peer will add as single entry ( +1 )
+			expectedCacheSize += 1
+			syncTester.ExpectCacheSize(expectedCacheSize)
+			syncTester.ExpectPath("/calico/resources/v3/projectcalico.org/externalnetwork/net1")
+
 			// For non-kubernetes, check that we can allocate an IP address and get a syncer update
 			// for the allocation block.
 			var blockAffinityKeyV1 model.BlockAffinityKey
