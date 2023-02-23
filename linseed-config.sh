e780c0a5dc9613b24b5d6d4d963d6db0e9987076
Alina Militaru <41362174+asincu@users.noreply.github.com>
2023-02-23 09:07:46 -0800

    Add ingestion for DNS, L3, L7, AUDIT and BGP via bulk apis (#334)
    Use Linseed as an ingestion point instead of ElasticSearch. Bulk APIs
    require a POST request with application/x-ndjson set as content-type.
    Each log line will be correct json delimited by newline.

--- linseed-config.sh
+++ linseed-config.sh
@@ -1,5 +1,10 @@
 # Append additional output matcher config (for IDS events) when SYSLOG forwarding is turned on
 if [ "${LINSEED_ENABLED}" == "true" ]; then
   cp ${ROOT_DIR}/fluentd/etc/outputs/out-linseed-flows.conf ${ROOT_DIR}/fluentd/etc/output_flows/out-linseed-flows.conf
+  cp ${ROOT_DIR}/fluentd/etc/outputs/out-linseed-l7.conf ${ROOT_DIR}/fluentd/etc/output_l7/out-linseed-l7.conf
+  cp ${ROOT_DIR}/fluentd/etc/outputs/out-linseed-dns.conf ${ROOT_DIR}/fluentd/etc/output_dns/out-linseed-dns.conf
+  cp ${ROOT_DIR}/fluentd/etc/outputs/out-linseed-kube-audit.conf ${ROOT_DIR}/fluentd/etc/output_kube_audit/out-linseed-kube-audit.conf
+  cp ${ROOT_DIR}/fluentd/etc/outputs/out-linseed-ee-audit.conf ${ROOT_DIR}/fluentd/etc/output_tsee_audit/out-linseed-ee-audit.conf
+  cp ${ROOT_DIR}/fluentd/etc/outputs/out-linseed-bgp.conf ${ROOT_DIR}/fluentd/etc/output_bgp/out-linseed-bgp.conf
 fi
 
