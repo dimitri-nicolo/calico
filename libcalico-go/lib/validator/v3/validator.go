a8f5ee88e345ce33ccddd883c96f974860fb649e
matt <mleung975@gmail.com>
2023-04-07 17:04:09 -0700

    Fix duplicate code from merge

--- libcalico-go/lib/validator/v3/validator.go
+++ libcalico-go/lib/validator/v3/validator.go
@@ -187,8 +187,6 @@ var (
 	L7ResponseCodeAggregationRegex   = regexp.MustCompile("^(IncludeL7ResponseCode|ExcludeL7ResponseCode)$")
 	L7URLAggregationRegex            = regexp.MustCompile("^(IncludeL7FullURL|TrimURLQuery|TrimURLQueryAndPath|ExcludeL7URL)$")
 
-	filterActionRegex    = regexp.MustCompile("^(Accept|Reject)$")
-	matchOperatorRegex   = regexp.MustCompile("^(Equal|In|NotEqual|NotIn)$")
 	enabledDisabledRegex = regexp.MustCompile("^(Enabled|Disabled)$")
 
 	ipv4LinkLocalNet = net.IPNet{
