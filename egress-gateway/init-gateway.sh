0b4f91aba7f96f4154f45cdbae12f8c28ec1a2ed
sridhar <sridhar@tigera.io>
2023-04-24 06:56:21 -0700

    Auto-detect iptables backend

--- egress-gateway/init-gateway.sh
+++ egress-gateway/init-gateway.sh
@@ -12,12 +12,6 @@ trap 'echo "INT received, simply exiting..."; exit 0' INT
 trap 'echo "TERM received, simply exiting..."; exit 0' TERM
 trap 'echo "HUP received, simply exiting..."; exit 0' HUP
 
-# IPTABLES_BACKEND may be set to "legacy" (default) or "nft".  The
-# "legacy" default should always work, until Linux is eventually
-# modified not to support the legacy mode any more.
-: ${IPTABLES_BACKEND:=legacy}
-iptables=iptables-${IPTABLES_BACKEND}
-
 if [ -z "$EGRESS_POD_IP" ]
 then
     echo "EGRESS_POD_IP not defined."
@@ -41,7 +35,24 @@ MAC=`echo $EGRESS_POD_IP | awk -F. '{printf "a2:2a:%02x:%02x:%02x:%02x", $1, $2,
 echo Egress VXLAN VNI: $EGRESS_VXLAN_VNI  VXLAN PORT: $EGRESS_VXLAN_PORT VXLAN MAC: $MAC Pod IP: $EGRESS_POD_IP
 
 echo Configure iptable rules
-${iptables} -t nat -A POSTROUTING -j MASQUERADE
+
+# IPTABLES_BACKEND may be set to "legacy" or "nft".  If not defined
+# try to auto-detect nft or legacy.
+
+if [ "$IPTABLES_BACKEND" ]
+then
+    echo "IPTABLES_BACKEND set to $IPTABLES_BACKEND"
+    iptables-${IPTABLES_BACKEND} -t nat -A POSTROUTING -j MASQUERADE
+elif iptables-nft -t nat -A POSTROUTING -j MASQUERADE
+then
+    echo "Successfully configured iptables with iptables-nft."
+elif iptables-legacy -t nat -A POSTROUTING -j MASQUERADE
+then
+    echo "Successfully configured iptables with iptables-legacy."
+else
+    echo "Failed to configure iptables (tried both nft and legacy)."
+    exit 1
+fi
 
 echo Configure vxlan tunnel device
 ip link add vxlan0 type vxlan id $EGRESS_VXLAN_VNI dstport $EGRESS_VXLAN_PORT dev eth0 || printf " (and that's fine)"
