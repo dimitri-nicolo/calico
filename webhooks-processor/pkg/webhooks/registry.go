28676ce55760dcb161dc04cf182381973e02207d
Antony Guinard <antony@tigera.io>
2023-10-17 10:18:06 +0100

    Make provider configuration configurable through env vars

--- webhooks-processor/pkg/webhooks/registry.go
+++ webhooks-processor/pkg/webhooks/registry.go
@@ -5,6 +5,7 @@ package webhooks
 import (
 	"time"
 
+	"github.com/kelseyhightower/envconfig"
 	api "github.com/tigera/api/pkg/apis/projectcalico/v3"
 
 	"github.com/projectcalico/calico/webhooks-processor/pkg/providers/generic"
@@ -18,23 +19,59 @@ type ProviderConfiguration struct {
 	RateLimiterCount    uint
 }
 
+type JiraProviderConfiguration struct {
+	RateLimiterDuration time.Duration `envconfig:"WEBHOOKS_JIRA_RATE_LIMITER_DURATION" default:"60m"`
+	RateLimiterCount    uint          `envconfig:"WEBHOOKS_JIRA_RATE_LIMITER_COUNT" default:"1"`
+}
+
+type SlackProviderConfiguration struct {
+	RateLimiterDuration time.Duration `envconfig:"WEBHOOKS_SLACK_RATE_LIMITER_DURATION" default:"5m"`
+	RateLimiterCount    uint          `envconfig:"WEBHOOKS_SLACK_RATE_LIMITER_COUNT" default:"3"`
+}
+
+type GenericProviderConfiguration struct {
+	RateLimiterDuration time.Duration `envconfig:"WEBHOOKS_GENERIC_RATE_LIMITER_DURATION" default:"1h"`
+	RateLimiterCount    uint          `envconfig:"WEBHOOKS_GENERIC_RATE_LIMITER_COUNT" default:"100"`
+}
+
+func NewJiraProviderConfiguration() *ProviderConfiguration {
+	config := new(JiraProviderConfiguration)
+	envconfig.MustProcess("webhooks", config)
+
+	return &ProviderConfiguration{
+		Provider:            jira.NewProvider(),
+		RateLimiterDuration: config.RateLimiterDuration,
+		RateLimiterCount:    config.RateLimiterCount,
+	}
+}
+
+func NewSlackProviderConfiguration() *ProviderConfiguration {
+	config := new(SlackProviderConfiguration)
+	envconfig.MustProcess("webhooks", config)
+
+	return &ProviderConfiguration{
+		Provider:            slack.NewProvider(),
+		RateLimiterDuration: config.RateLimiterDuration,
+		RateLimiterCount:    config.RateLimiterCount,
+	}
+}
+
+func NewGenericProviderConfiguration() *ProviderConfiguration {
+	config := new(GenericProviderConfiguration)
+	envconfig.MustProcess("webhooks", config)
+
+	return &ProviderConfiguration{
+		Provider:            generic.NewProvider(),
+		RateLimiterDuration: config.RateLimiterDuration,
+		RateLimiterCount:    config.RateLimiterCount,
+	}
+}
+
 func DefaultProviders() map[api.SecurityEventWebhookConsumer]*ProviderConfiguration {
 	RegisteredProviders := make(map[api.SecurityEventWebhookConsumer]*ProviderConfiguration)
-	RegisteredProviders[api.SecurityEventWebhookConsumerJira] = &ProviderConfiguration{
-		Provider:            jira.NewProvider(),
-		RateLimiterDuration: 60 * time.Minute,
-		RateLimiterCount:    1,
-	}
-	RegisteredProviders[api.SecurityEventWebhookConsumerSlack] = &ProviderConfiguration{
-		Provider:            slack.NewProvider(),
-		RateLimiterDuration: 5 * time.Minute,
-		RateLimiterCount:    3,
-	}
-	RegisteredProviders[api.SecurityEventWebhookConsumerGeneric] = &ProviderConfiguration{
-		Provider:            generic.NewProvider(),
-		RateLimiterDuration: 1 * time.Hour,
-		RateLimiterCount:    100,
-	}
+	RegisteredProviders[api.SecurityEventWebhookConsumerJira] = NewJiraProviderConfiguration()
+	RegisteredProviders[api.SecurityEventWebhookConsumerSlack] = NewSlackProviderConfiguration()
+	RegisteredProviders[api.SecurityEventWebhookConsumerGeneric] = NewGenericProviderConfiguration()
 
 	return RegisteredProviders
 }
