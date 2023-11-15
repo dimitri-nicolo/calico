acdc120946be9d037767d422ec6e99f0f429630a
Shaun Crampton <shaun@tigera.io>
2023-11-15 10:21:07 +0000

    Add needed libs to clean-up-filesystem.sh

--- egress-gateway/clean-up-filesystem.sh
+++ egress-gateway/clean-up-filesystem.sh
@@ -168,6 +168,7 @@ done < <(find /usr/lib64 \( -type f -or -type l \))
 # binaries and libraries that we don't want.
 packages_to_keep=(
   bash
+  bzip2-libs
   ca-certificates
   conntrack-tools
   coreutils-single
@@ -196,6 +197,8 @@ packages_to_keep=(
   libselinux
   libsigsegv
   libunistring
+  libzstd
+  libz
   mpfr
   ncurses
   openssl-libs
