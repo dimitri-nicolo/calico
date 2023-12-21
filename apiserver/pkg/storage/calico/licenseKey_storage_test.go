527dbf58cf87baa05e6851384b4c677888f86d23
Alina Militaru <41362174+asincu@users.noreply.github.com>
2023-12-20 17:14:04 -0800

    [EV-4474] Expired license does not show expiry via kubectl get (#7047)
    * [EV-4474] Expired license does not show expiry via kubectl get
    
    * Fix! Mount secrets for uts

---
+++ apiserver/pkg/storage/calico/licenseKey_storage_test.go
@@ -0,0 +1,51 @@
+package calico
+
+import (
+	"testing"
+
+	v3 "github.com/tigera/api/pkg/apis/projectcalico/v3"
+
+	"github.com/projectcalico/calico/licensing/utils"
+)
+
+func TestLicenseKeyConverter_convertToAAPI(t *testing.T) {
+	tests := []struct {
+		name         string
+		givenLicense resourceObject
+	}{
+		{name: "expired license", givenLicense: utils.ExpiredTestLicense()},
+		{name: "enterprise license", givenLicense: utils.ValidEnterpriseTestLicense()},
+		{name: "cloud license", givenLicense: utils.ValidCloudProTestLicense()},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gc := LicenseKeyConverter{}
+			licenseKey := v3.LicenseKey{}
+
+			gc.convertToAAPI(tt.givenLicense, &licenseKey)
+			if isEmpty(licenseKey.Status) {
+				t.Errorf("License status cannot be empty %v", licenseKey.Status)
+			}
+		})
+	}
+}
+
+func isEmpty(status v3.LicenseKeyStatus) bool {
+	if status.Expiry.IsZero() {
+		return true
+	}
+
+	if len(status.Features) == 0 {
+		return true
+	}
+
+	if status.MaxNodes == 0 {
+		return true
+	}
+
+	if status.Package == "" {
+		return true
+	}
+
+	return false
+}
