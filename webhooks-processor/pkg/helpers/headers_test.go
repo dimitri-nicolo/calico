1b7d6cfffd46f62e046252d70f01975b7204d615
Bart Biskupek <bart@tigera.io>
2024-03-13 11:56:16 +0000

    basic http headers validation for the generic webhook.

--- webhooks-processor/pkg/helpers/headers_test.go
+++ webhooks-processor/pkg/helpers/headers_test.go
@@ -13,18 +13,30 @@ import (
 func TestProcessHeaders(t *testing.T) {
 	// empty corner case:
 	rawHeaders := ""
-	headers := helpers.ProcessHeaders(rawHeaders)
+	headers, err := helpers.ProcessHeaders(rawHeaders)
+	assert.NoError(t, err)
 	assert.Equal(t, headers, map[string]string{})
 
-	// arbitrary data:
+	// valid arbitrary data:
 	rawHeaders = `
 this is not a header line and will be ignored as well as the next empty line
 
 this-is-a-header:with some data in it
 this-is-another-header:with-data-and-colon:like-that`
-	headers = helpers.ProcessHeaders(rawHeaders)
+	headers, err = helpers.ProcessHeaders(rawHeaders)
+	assert.NoError(t, err)
 	assert.Equal(t, headers, map[string]string{
 		"this-is-a-header":       "with some data in it",
 		"this-is-another-header": "with-data-and-colon:like-that",
 	})
+
+	// invalid header name
+	rawHeaders = "this-is-a-`header`:with invalid characters in its name"
+	_, err = helpers.ProcessHeaders(rawHeaders)
+	assert.Error(t, err)
+
+	// invalid header value
+	rawHeaders = "this-is-a-header:with invalid characters in its value ™"
+	_, err = helpers.ProcessHeaders(rawHeaders)
+	assert.Error(t, err)
 }
