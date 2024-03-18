c147053f0119c274bcfe6d8945d595e8674c2dee
sridhar <sridhar@tigera.io>
2024-03-18 06:25:29 -0700

    Fix dual tor tests

--- node/tests/k8st/external-network/external-networks.sh
+++ node/tests/k8st/external-network/external-networks.sh
@@ -4,7 +4,7 @@
 TEST_DIR=./tests/k8st
 
 # kubectl binary.
-: ${kubectl:=./kubectl}
+: ${kubectl:=../hack/test/kind/kubectl}
 
 # Normally, cleanup any leftover state, then setup, then test.
 : ${STEPS:=cleanup setup}
