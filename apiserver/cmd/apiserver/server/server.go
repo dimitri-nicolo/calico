9c8d240d2e1bd440cbb815d6a65fc6d0bf4a611c
sridhar <sridhar@tigera.io>
2024-04-25 14:29:58 -0700

    Fix apiserver build

--- apiserver/cmd/apiserver/server/server.go
+++ apiserver/cmd/apiserver/server/server.go
@@ -25,6 +25,9 @@ import (
 	"github.com/spf13/cobra"
 	v3 "github.com/tigera/api/pkg/apis/projectcalico/v3"
 	genericoptions "k8s.io/apiserver/pkg/server/options"
+
+	"github.com/projectcalico/calico/libcalico-go/lib/logutils"
+	"github.com/sirupsen/logrus"
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/util/interrupt"
 )
