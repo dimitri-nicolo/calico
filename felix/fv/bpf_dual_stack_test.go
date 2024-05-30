ac516b2f07a1d44d69004d9c092a34a003fa4cf8
sridhartigera <63839878+sridhartigera@users.noreply.github.com>
2024-05-29 20:01:32 -0700

    Interface autodetection - ebpf (#8803)

--- felix/fv/bpf_dual_stack_test.go
+++ felix/fv/bpf_dual_stack_test.go
@@ -335,8 +335,8 @@ func describeBPFDualStackTests(ctlbEnabled, ipv6Dataplane bool) bool {
 
 					tc.TriggerDelayedStart()
 
-					ensureRightIFStateFlags(tc.Felixes[0], ifstate.FlgIPv4Ready)
-					ensureRightIFStateFlags(tc.Felixes[1], ifstate.FlgIPv4Ready|ifstate.FlgIPv6Ready)
+					ensureRightIFStateFlags(tc.Felixes[0], ifstate.FlgIPv4Ready, nil)
+					ensureRightIFStateFlags(tc.Felixes[1], ifstate.FlgIPv4Ready|ifstate.FlgIPv6Ready, nil)
 					cc.ExpectSome(w[0][1], w[0][0])
 					cc.ExpectSome(w[1][0], w[0][0])
 					cc.ExpectSome(w[1][1], w[0][0])
@@ -368,7 +368,7 @@ func describeBPFDualStackTests(ctlbEnabled, ipv6Dataplane bool) bool {
 					_, err = k8sClient.CoreV1().Nodes().UpdateStatus(context.Background(), node, metav1.UpdateOptions{})
 					Expect(err).NotTo(HaveOccurred())
 
-					ensureRightIFStateFlags(tc.Felixes[0], ifstate.FlgIPv4Ready|ifstate.FlgIPv6Ready)
+					ensureRightIFStateFlags(tc.Felixes[0], ifstate.FlgIPv4Ready|ifstate.FlgIPv6Ready, nil)
 					cc.ExpectSome(w[0][1], w[0][0])
 					cc.ExpectSome(w[1][0], w[0][0])
 					cc.ExpectSome(w[1][1], w[0][0])
@@ -383,8 +383,8 @@ func describeBPFDualStackTests(ctlbEnabled, ipv6Dataplane bool) bool {
 					tc.TriggerDelayedStart()
 					externalClient := infrastructure.RunExtClient("ext-client")
 					_ = externalClient
-					ensureRightIFStateFlags(tc.Felixes[0], ifstate.FlgIPv4Ready|ifstate.FlgIPv6Ready)
-					ensureRightIFStateFlags(tc.Felixes[1], ifstate.FlgIPv4Ready|ifstate.FlgIPv6Ready)
+					ensureRightIFStateFlags(tc.Felixes[0], ifstate.FlgIPv4Ready|ifstate.FlgIPv6Ready, nil)
+					ensureRightIFStateFlags(tc.Felixes[1], ifstate.FlgIPv4Ready|ifstate.FlgIPv6Ready, nil)
 
 					tcpdump := externalClient.AttachTCPDump("any")
 					tcpdump.SetLogEnabled(true)
@@ -406,8 +406,8 @@ func describeBPFDualStackTests(ctlbEnabled, ipv6Dataplane bool) bool {
 		} else {
 			JustBeforeEach(func() {
 				tc.TriggerDelayedStart()
-				ensureRightIFStateFlags(tc.Felixes[0], ifstate.FlgIPv4Ready)
-				ensureRightIFStateFlags(tc.Felixes[1], ifstate.FlgIPv4Ready)
+				ensureRightIFStateFlags(tc.Felixes[0], ifstate.FlgIPv4Ready, nil)
+				ensureRightIFStateFlags(tc.Felixes[1], ifstate.FlgIPv4Ready, nil)
 			})
 			It("should drop ipv6 packets at workload interface and allow ipv6 packets at host interface when in IPv4 only mode", func() {
 				// IPv4 connectivity must work.
@@ -423,10 +423,17 @@ func describeBPFDualStackTests(ctlbEnabled, ipv6Dataplane bool) bool {
 	})
 }
 
-func ensureRightIFStateFlags(felix *infrastructure.Felix, ready uint32) {
+func ensureRightIFStateFlags(felix *infrastructure.Felix, ready uint32, additionalInterfaces map[string]uint32) {
 	expectedIfacesToFlags := map[string]uint32{
-		"eth0": ready,
+		"eth0": ifstate.FlgHEP | ready,
 	}
+
+	if additionalInterfaces != nil {
+		for k, v := range additionalInterfaces {
+			expectedIfacesToFlags[k] = v
+		}
+	}
+
 	for _, w := range felix.Workloads {
 		if w.Runs() {
 			if iface := w.GetInterfaceName(); iface != "" {
