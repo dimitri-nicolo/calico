b9f61a9a00b84f9f5c569fc954287513c4aa08d3
Shaun Crampton <shaun@tigera.io>
2024-08-09 15:46:14 +0100

    Fix resizing of BPF maps.
    Maps were being correctly resized but the program attachment
    metadata wasn't updated so the programs failed to attach.
    
    - Fix the test to actually verify basic workload connectivity.
    - Add XDP version of test.
    - Add metadta updates to each of the attach points.

--- felix/bpf/tc/attach.go
+++ felix/bpf/tc/attach.go
@@ -34,6 +34,7 @@ import (
 	"github.com/projectcalico/calico/felix/bpf/bpfdefs"
 	"github.com/projectcalico/calico/felix/bpf/hook"
 	"github.com/projectcalico/calico/felix/bpf/libbpf"
+	"github.com/projectcalico/calico/felix/bpf/maps"
 	tcdefs "github.com/projectcalico/calico/felix/bpf/tc/defs"
 )
 
@@ -110,6 +111,12 @@ func (ap *AttachPoint) loadObject(file string) (*libbpf.Obj, error) {
 				return nil, fmt.Errorf("failed to configure %s: %w", file, err)
 			}
 			continue
+		}
+
+		if size := maps.Size(mapName); size != 0 {
+			if err := m.SetSize(size); err != nil {
+				return nil, fmt.Errorf("error resizing map %s: %w", mapName, err)
+			}
 		}
 
 		log.Debugf("Pinning map %s k %d v %d", mapName, m.KeySize(), m.ValueSize())
