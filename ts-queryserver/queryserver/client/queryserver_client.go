27ff411cb6cd52e60a87599ec0f63e148d42078b
Alina Militaru <41362174+asincu@users.noreply.github.com>
2024-09-09 16:38:38 -0700

    [TSLA-7749] Remove impersonation headers for multi-tenancy

--- ts-queryserver/queryserver/client/queryserver_client.go
+++ ts-queryserver/queryserver/client/queryserver_client.go
@@ -17,11 +17,10 @@ import (
 )
 
 type QueryServerConfig struct {
-	QueryServerTunnelURL    string
-	QueryServerURL          string
-	QueryServerCA           string
-	QueryServerToken        string
-	AddImpersonationHeaders bool
+	QueryServerTunnelURL string
+	QueryServerURL       string
+	QueryServerCA        string
+	QueryServerToken     string
 }
 
 var errInvalidToken = errors.New("queryServer Token is not valid")
@@ -32,8 +31,7 @@ type QueryServerClient interface {
 }
 
 type queryServerClient struct {
-	client                  *http.Client
-	addImpersonationHeaders bool
+	client *http.Client
 }
 
 type QueryServerResults struct {
@@ -65,8 +63,7 @@ func NewQueryServerClient(cfg *QueryServerConfig) (*queryServerClient, error) {
 	}
 
 	return &queryServerClient{
-		client:                  client,
-		addImpersonationHeaders: cfg.AddImpersonationHeaders,
+		client: client,
 	}, nil
 }
 
@@ -98,17 +95,6 @@ func (q *queryServerClient) SearchEndpoints(cfg *QueryServerConfig, reqBody *que
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", cfg.QueryServerToken))
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("x-cluster-id", clusterId)
-
-	if q.addImpersonationHeaders {
-		// This is a multi-tenant management cluster. In this setup, in order to fetch information
-		// from the managed cluster, we must impersonate the canonical service account
-		// The Bearer token stores a service account created with a tenant namespace, but we need
-		// to use the canonical service account tigera-manager from tigera-manager namespace
-		req.Header.Add("Impersonate-User", "system:serviceaccount:tigera-manager:tigera-manager")
-		req.Header.Add("Impersonate-Group", "system:authenticated")
-		req.Header.Add("Impersonate-Group", "system:serviceaccounts")
-		req.Header.Add("Impersonate-Group", "system:serviceaccounts:tigera-manager")
-	}
 
 	resp, err := q.client.Do(req)
 	if err != nil {
--- ts-queryserver/queryserver/client/queryserver_client_test.go
+++ ts-queryserver/queryserver/client/queryserver_client_test.go
@@ -1,19 +1,8 @@
 package client
 
 import (
-	"bytes"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
-	"fmt"
-	"math/big"
 	"net/http"
 	"net/http/httptest"
-	"os"
-	"path/filepath"
-	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -81,60 +70,6 @@ var _ = Describe("QuerysServerClient tests", func() {
 			Expect(resp.Count).To(Equal(0))
 		})
 
-		It("multi-tenant management cluster making a request for a managed cluster", func() {
-			// Create a temporary directory to store file dependencies
-			tempDir, err := os.MkdirTemp("", "query-client")
-			if err != nil {
-				fmt.Println("Error creating temporary file:", err)
-				return
-			}
-			defer func(path string) {
-				_ = os.RemoveAll(path)
-			}(tempDir)
-
-			// Write CA
-			err = writeCA(tempDir)
-			Expect(err).ShouldNot(HaveOccurred())
-
-			// Write token
-			err = writeToken(tempDir)
-			Expect(err).ShouldNot(HaveOccurred())
-
-			// Create a mock server that enforces the checks for impersonation headers
-			server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				defer GinkgoRecover()
-
-				Expect(r.Header.Get("Impersonate-User")).To(Equal("system:serviceaccount:tigera-manager:tigera-manager"))
-				Expect(r.Header.Values("Impersonate-Group")).To(ContainElements(
-					"system:authenticated",
-					"system:serviceaccounts",
-					"system:serviceaccounts:tigera-manager",
-				))
-
-				w.WriteHeader(http.StatusOK)
-				_, err := w.Write([]byte(`{"count": 0, "items": []}`))
-				Expect(err).ShouldNot(HaveOccurred())
-			}))
-
-			defer server.Close()
-
-			config := &QueryServerConfig{
-				QueryServerTunnelURL:    server.URL,
-				QueryServerURL:          "",
-				QueryServerCA:           filepath.Join(tempDir, "tigera-ca-bundle.crt"),
-				QueryServerToken:        filepath.Join(tempDir, "token"),
-				AddImpersonationHeaders: true,
-			}
-
-			client, err := NewQueryServerClient(config)
-			Expect(err).ShouldNot(HaveOccurred())
-
-			body := &querycacheclient.QueryEndpointsReqBody{}
-			resp, err := client.SearchEndpoints(config, body, "managedcluster")
-			Expect(err).ShouldNot(HaveOccurred())
-			Expect(resp.Count).To(Equal(0))
-		})
-
 		It("query server token is empty", func() {
 			config := &QueryServerConfig{
 				QueryServerTunnelURL: "",
@@ -172,94 +107,3 @@ var _ = Describe("QuerysServerClient tests", func() {
 		})
 	})
 })
-
-func writeToken(tempDir string) error {
-	tokenValue := []byte("any")
-	tokenFilePath := filepath.Join(tempDir, "token")
-	tokenFile, err := os.Create(tokenFilePath)
-	if err != nil {
-		return err
-	}
-	write, err := tokenFile.Write(tokenValue)
-	if err != nil {
-		return err
-	}
-	if write != len(tokenValue) {
-		return fmt.Errorf("failed to write all token")
-	}
-	err = tokenFile.Close()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func writeCA(tempDir string) error {
-	ca, caKey, err := createCAKeyPair()
-	if err != nil {
-		return err
-	}
-	caBytes, err := signAndEncodeCert(ca, caKey, ca, caKey)
-	if err != nil {
-		return err
-	}
-	caFilePath := filepath.Join(tempDir, "tigera-ca-bundle.crt")
-	caFile, err := os.Create(caFilePath)
-	if err != nil {
-		return err
-	}
-	write, err := caFile.Write(caBytes)
-	if err != nil {
-		return err
-	}
-	if write != len(caBytes) {
-		return fmt.Errorf("expected to write all bytes to the CA file")
-	}
-	err = caFile.Close()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func createCAKeyPair() (*x509.Certificate, *rsa.PrivateKey, error) {
-	// Create a x509 template for the mustCreateCAKeyPair
-	template := &x509.Certificate{
-		SerialNumber: big.NewInt(1658),
-		Subject: pkix.Name{
-			Organization: []string{"Tigera"},
-		},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(10, 0, 0),
-		IsCA:                  true,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-		BasicConstraintsValid: true,
-	}
-
-	// Generate a private key
-	key, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return template, key, nil
-}
-
-func signAndEncodeCert(ca *x509.Certificate, caPrivateKey *rsa.PrivateKey, cert *x509.Certificate, key *rsa.PrivateKey) ([]byte, error) {
-	// Sign the certificate with the provided CA
-	certBytes, err := x509.CreateCertificate(rand.Reader, cert, ca, &key.PublicKey, caPrivateKey)
-	if err != nil {
-		return nil, err
-	}
-
-	// Encode the certificate
-	certPEM := bytes.Buffer{}
-	err = pem.Encode(&certPEM, &pem.Block{Type: "CERTIFICATE", Bytes: certBytes})
-	if err != nil {
-		return nil, err
-	}
-
-	return certPEM.Bytes(), nil
-}
