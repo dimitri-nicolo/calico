cacfb5fea20d77b96d667517d3393914c1d709d9
sridhar <sridhar@tigera.io>
2024-11-17 18:02:13 -0800

    Fix DNS fv

--- felix/fv/dns_scapy_test.go
+++ felix/fv/dns_scapy_test.go
@@ -497,12 +497,12 @@ var _ = Describe("_BPF-SAFE_ DNS Policy", func() {
 					Expect(err).NotTo(HaveOccurred())
 
 					// Allow 2s for Felix to see and process that policy.
-					time.Sleep(2 * time.Second)
-
 					if BPFMode() {
 						Eventually(func() bool {
 							return bpfCheckIfPolicyProgrammed(tc.Felixes[0], w[0].InterfaceName, "egress", "default.allow-xyz", "allow", true)
-						}, "2s", "200ms").Should(BeTrue())
+						}, "10s", "200ms").Should(BeTrue())
+					} else {
+						time.Sleep(2 * time.Second)
 					}
 
 					// We use the ping target container as a target IP for the workload to ping, so
