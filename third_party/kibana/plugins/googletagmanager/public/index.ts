f3d3baa31e7134d87c8219ba575f58acb1a7cb79
vara <vara@tigera.io>
2025-02-27 13:50:04 -0800

    regenerate googletagmanger plugin to align with cloud kibana.yml
    Fix GTM_INTEGERATION FLAG check to include gtm, regenerate gtm plugin
    to align gtm kibana config name with existing cloud config

--- third_party/kibana/plugins/googletagmanager/public/index.ts
+++ third_party/kibana/plugins/googletagmanager/public/index.ts
@@ -1,10 +1,10 @@
 import { PluginInitializerContext } from '@kbn/core/public';
-import { GoogleTagManagerPlugin } from './plugin';
+import { GoogletagmanagerPlugin } from './plugin';
 import { ConfigSchema} from '../common';
 
 // This exports static code and TypeScript types,
 // as well as, Kibana Platform `plugin()` initializer.
 export function plugin(initializerContext: PluginInitializerContext<ConfigSchema>) {
-  return new GoogleTagManagerPlugin(initializerContext);
+  return new GoogletagmanagerPlugin(initializerContext);
 }
 
