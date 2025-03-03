0a7159f081d1424a1288cb696e32fc1183d34178
Casey Davenport <davenport.cas@gmail.com>
2025-03-03 15:11:02 -0800

    Fix a couple of tests

--- libcalico-go/lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
+++ libcalico-go/lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
@@ -45,7 +45,7 @@ const (
 
 const (
 	// numBaseFelixConfigs should match the OS value.
-	numBaseFelixConfigs = 161
+	numBaseFelixConfigs = 156
 
 	// Add private-only fields to this count.
 	numPrivateOnlyFelixConfigs = 117
