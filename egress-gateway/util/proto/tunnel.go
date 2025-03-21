c48463e1af6fc3b2e6de6ad7242c482243eb0529
sridhar <sridhar@tigera.io>
2025-03-20 19:15:49 -0700

    Fix EGW tests

--- egress-gateway/util/proto/tunnel.go
+++ egress-gateway/util/proto/tunnel.go
@@ -8,31 +8,31 @@ func IsRouteType(ru *proto.RouteUpdate, t proto.RouteType) bool {
 
 // IsWireguardTunnel returns true if a route update represents a remote host's WireGuard tunnel
 func IsWireguardTunnel(ru *proto.RouteUpdate) bool {
-	return IsRouteType(ru, proto.RouteType_REMOTE_TUNNEL) ||
-		IsRouteType(ru, proto.RouteType_LOCAL_TUNNEL) &&
+	return (IsRouteType(ru, proto.RouteType_REMOTE_TUNNEL) ||
+		IsRouteType(ru, proto.RouteType_LOCAL_TUNNEL)) &&
 			ru.TunnelType != nil &&
 			ru.TunnelType.Wireguard
 }
 
 // IsVXLANTunnel returns true if a route update represents a remote host's VXLAN tunnel
 func IsVXLANTunnel(ru *proto.RouteUpdate) bool {
-	return IsRouteType(ru, proto.RouteType_REMOTE_TUNNEL) ||
-		IsRouteType(ru, proto.RouteType_LOCAL_TUNNEL) &&
+	return (IsRouteType(ru, proto.RouteType_REMOTE_TUNNEL) ||
+		IsRouteType(ru, proto.RouteType_LOCAL_TUNNEL)) &&
 			ru.TunnelType != nil &&
 			ru.TunnelType.Vxlan
 }
 
 // IsIPIPTunnel returns true if a route update represents a remote host's IPIP tunnel
 func IsIPIPTunnel(ru *proto.RouteUpdate) bool {
-	return IsRouteType(ru, proto.RouteType_REMOTE_TUNNEL) ||
-		IsRouteType(ru, proto.RouteType_LOCAL_TUNNEL) &&
+	return (IsRouteType(ru, proto.RouteType_REMOTE_TUNNEL) ||
+		IsRouteType(ru, proto.RouteType_LOCAL_TUNNEL)) &&
 			ru.TunnelType != nil &&
 			ru.TunnelType.Ipip
 }
 
 // IsHostTunnel determines if a route update represents a tunnel device in a remote host
 func IsHostTunnel(ru *proto.RouteUpdate) bool {
-	return IsRouteType(ru, proto.RouteType_REMOTE_TUNNEL) ||
-		IsRouteType(ru, proto.RouteType_LOCAL_TUNNEL) &&
+	return (IsRouteType(ru, proto.RouteType_REMOTE_TUNNEL) ||
+		IsRouteType(ru, proto.RouteType_LOCAL_TUNNEL)) &&
 			ru.TunnelType != nil
 }
